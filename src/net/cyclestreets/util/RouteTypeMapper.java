db9841abda486f7dfc4fd5c4a307206d908b1308
Jez Higgins <jez@jezuk.co.uk>
2011-01-15 00:02:21 +0000

    Add RouteTypeMapper to go from button ids to route type names and back.  Use it simplify route button callback, and set the route type button from the preference.

---
+++ src/net/cyclestreets/util/RouteTypeMapper.java
@@ -0,0 +1,34 @@
+package net.cyclestreets.util;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import net.cyclestreets.CycleStreetsConstants;
+import net.cyclestreets.R;
+
+public final class RouteTypeMapper {
+	static private final Map<Integer, String> mappings = initialiseMappings();
+	
+	static private Map<Integer, String> initialiseMappings() {
+		final Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(R.id.routeQuietest, CycleStreetsConstants.PLAN_QUIETEST);
+		map.put(R.id.routeBalanced, CycleStreetsConstants.PLAN_BALANCED);
+		map.put(R.id.routeFastest, CycleStreetsConstants.PLAN_FASTEST);
+		return map;
+	} // initialiseMappings
+
+	private RouteTypeMapper() {
+		// prevent instantiation
+	} // RouteTypeMapper
+	
+	static public String nameFromId(int id) {
+		return mappings.get(id);
+	} // nameFromId
+	
+	static public int idFromName(final String name) {
+		for(final Map.Entry<Integer,String> entry : mappings.entrySet())
+			if(entry.getValue().equals(name))
+				return entry.getKey();
+		return -1;
+	}
+} // class RouteTypeMapper
