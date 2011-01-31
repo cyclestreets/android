006a3a23e928eb9ba9e6671045827c74832e35e7
Jez Higgins <jez@jezuk.co.uk>
2011-01-31 13:48:30 +0000

    OverlayButton - added pressed state, not entirely convinced by the UI yet though

--- src/net/cyclestreets/overlay/OverlayButton.java
+++ src/net/cyclestreets/overlay/OverlayButton.java
@@ -2,6 +2,7 @@ package net.cyclestreets.overlay;
 
 import net.cyclestreets.util.Brush;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
@@ -13,6 +14,7 @@ class OverlayButton
 	private final Rect pos_;
 	private final float radius_;
 	private boolean enabled_;
+	private boolean pressed_;
 	
 	public OverlayButton(final Drawable image, final int left, final int top, final float curveRadius)
 	{
@@ -23,9 +25,11 @@ class OverlayButton
         				top + image.getIntrinsicHeight());
         radius_ = curveRadius;
         enabled_ = true;
+        pressed_ = false;
 	} // OverlayButton
 	
 	public void enable(final boolean on) { enabled_ = on; }
+	public void pressed(final boolean on) { pressed_ = on; }
 	
 	public int right() { return pos_.right;	}
 	public int height() { return pos_.height(); }
@@ -36,12 +40,28 @@ class OverlayButton
         screen.offset(pos_.left, pos_.top);
         screen.right = screen.left + pos_.width();
         screen.bottom = screen.top + pos_.height();
-	        
-		canvas.drawRoundRect(new RectF(screen), radius_, radius_, enabled_ ? Brush.White : Brush.LightGrey);
+	    
+        final RectF rounded = new RectF(screen);
+		canvas.drawRoundRect(rounded, radius_, radius_, enabled_ ? Brush.White : Brush.LightGrey);
+		
+		if(enabled_ && pressed_)
+		{
+			shrinkAndDrawInner(canvas, rounded, Brush.LightGrey);
+			shrinkAndDrawInner(canvas, rounded, Brush.White);
+		} // if ...
 
         img_.setBounds(screen);
         img_.draw(canvas);
 	} // drawButton
+	
+	private void shrinkAndDrawInner(final Canvas canvas, final RectF rect, final Paint brush)
+	{
+		rect.left += 4;
+		rect.top += 4;
+		rect.right -= 4;
+		rect.bottom -= 4;
+        canvas.drawRoundRect(rect, radius_, radius_, brush);	 
+	} // shrinkAndDrawInner
 	
 	public boolean hit(final MotionEvent event)
 	{
