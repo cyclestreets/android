8408f5dfe9435d62400f6aa7d79f2cd15f324ae1
Jez Higgins <jez@jezuk.co.uk>
2011-02-05 23:44:56 +0000

    CycleStreets Activity - removed labels from tabs, made tabs a bit smaller.  Trying to make more space for the maps.

--- src/net/cyclestreets/CycleStreets.java
+++ src/net/cyclestreets/CycleStreets.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.TabHost;
 
 public class CycleStreets extends TabActivity {
@@ -22,33 +23,40 @@ public class CycleStreets extends TabActivity {
 	    CycleStreetsPreferences.initialise(this);
 
         // initialize objects
-	    Resources res = getResources();
-	    TabHost tabHost = getTabHost();
+	    final Resources res = getResources();
+	    final TabHost tabHost = getTabHost();
 	    TabHost.TabSpec spec;
 	    
 	    // Plan route
-	    spec = tabHost.newTabSpec("Plan route").setIndicator("Plan Route", res.getDrawable(R.drawable.ic_tab_planroute));
+	    spec = tabHost.newTabSpec("Plan route").setIndicator("", res.getDrawable(R.drawable.ic_tab_planroute));
 	    spec.setContent(new Intent(this, MapActivity.class));
 	    tabHost.addTab(spec);
 
 	    // Itinerary
-	    spec = tabHost.newTabSpec("Itinerary").setIndicator("Itinerary", res.getDrawable(R.drawable.ic_tab_itinerary));
+	    spec = tabHost.newTabSpec("Itinerary").setIndicator("", res.getDrawable(R.drawable.ic_tab_itinerary));
 	    spec.setContent(new Intent(this, ItineraryActivity.class));
 	    tabHost.addTab(spec);
 
 	    // Photomap
-	    spec = tabHost.newTabSpec("Photomap").setIndicator("Photomap", res.getDrawable(R.drawable.ic_tab_photomap));
+	    spec = tabHost.newTabSpec("Photomap").setIndicator("", res.getDrawable(R.drawable.ic_tab_photomap));
 	    spec.setContent(new Intent(this, PhotomapActivity.class));
 	    tabHost.addTab(spec);
 
 	    // Add photo
-	    spec = tabHost.newTabSpec("Add photo").setIndicator("Add photo", res.getDrawable(R.drawable.ic_tab_addphoto));
+	    spec = tabHost.newTabSpec("Add photo").setIndicator("", res.getDrawable(R.drawable.ic_tab_addphoto));
 	    spec.setContent(new Intent(this, AddPhotoActivity.class));
 	    tabHost.addTab(spec);
 
-	    spec = tabHost.newTabSpec("More").setIndicator("More", res.getDrawable(R.drawable.ic_menu_info_details));
+	    spec = tabHost.newTabSpec("More").setIndicator("", res.getDrawable(R.drawable.ic_menu_info_details));
 	    spec.setContent(new Intent(this, MoreActivity.class));
 	    tabHost.addTab(spec);
+	    
+	    for(int i = 0; i != 5; ++i)
+	    {
+	    	final ViewGroup.LayoutParams layout = tabHost.getTabWidget().getChildAt(i).getLayoutParams();
+		    layout.height = (int)(layout.height*0.66);
+		    tabHost.getTabWidget().getChildAt(i).setLayoutParams(layout);
+	    } // for ...
 	    
 	    // start with route tab
 	    showMap();
