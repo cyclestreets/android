e46a0c2c741a72a15c12393f66c06cc187d56a78
Jez Higgins <jez@jezuk.co.uk>
2011-05-01 22:20:56 +0100

    Improved layout for account details screen

--- src/net/cyclestreets/AccountDetailsActivity.java
+++ src/net/cyclestreets/AccountDetailsActivity.java
@@ -2,6 +2,7 @@ package net.cyclestreets;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AccountDetailsActivity extends Activity 
 {
@@ -11,5 +12,16 @@ public class AccountDetailsActivity extends Activity
         super.onCreate(saved);
 
         setContentView(R.layout.accountdetails);
+        
+        setText(R.id.username, CycleStreetsPreferences.username());
+        setText(R.id.password, CycleStreetsPreferences.password());
     } // onCreate
+    
+	private void setText(final int id, final String value)
+	{
+		final TextView tv = (TextView)findViewById(id);
+		tv.setText(value);
+	} // setText
+
+
 } // class AccountDetailsActivity
