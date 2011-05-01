487cc04740b8598cba570d9f5c1ed1e489661d89
Jez Higgins <jez@jezuk.co.uk>
2011-05-01 22:03:51 +0100

    Started on custom username and password screen.

---
+++ src/net/cyclestreets/AccountDetailsActivity.java
@@ -0,0 +1,15 @@
+package net.cyclestreets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountDetailsActivity extends Activity 
+{
+    @Override
+    public void onCreate(final Bundle saved)
+    {
+        super.onCreate(saved);
+
+        setContentView(R.layout.accountdetails);
+    } // onCreate
+} // class AccountDetailsActivity
