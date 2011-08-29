39a1b2e0d2decfb82eb4b6240992995c9c1faa12
Jez Higgins <jez@jezuk.co.uk>
2011-08-29 22:59:00 +0100

    Added single line text edit dialog

--- src/net/cyclestreets/util/Dialog.java
+++ src/net/cyclestreets/util/Dialog.java
@@ -1,18 +1,23 @@
 package net.cyclestreets.util;
 
+import net.cyclestreets.R;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.EditText;
 
 public class Dialog 
 {
 	static public ProgressDialog createProgressDialog(final Context context,
-											   		   final int messageId)
+											   		  final int messageId)
 	{
 		return createProgressDialog(context, context.getString(messageId));
 	} // createProgressDialog
 	
 	static public ProgressDialog createProgressDialog(final Context context,
-											   final String message)
+													  final String message)
 	{
 		final ProgressDialog progress = new ProgressDialog(context);
 		progress.setMessage(message);
@@ -20,4 +25,34 @@ public class Dialog
 		progress.setCancelable(false);
 		return progress;
 	} // createProgressDialog
+	
+	public interface UpdatedTextListener
+	{
+		public void updatedText(final String updated);
+	} // interface UpdatedTextListener
+	
+	static public void editTextDialog(final Context context, 
+									  final String initialText,
+									  final String buttonText,
+									  final UpdatedTextListener listener)
+	{
+	    final View layout = View.inflate(context, R.layout.edittextdialog, null);
+	    final EditText textBox = ((EditText)layout.findViewById(R.id.edit_text));
+	    textBox.setText(initialText);
+
+	    final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+	    builder.setTitle("CycleStreets");
+
+	    builder.setPositiveButton(buttonText, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				final String t = textBox.getText().toString().trim();
+				listener.updatedText(t);
+			} // onClick
+		});
+	    builder.setView(layout);
+
+	    final AlertDialog ad = builder.create();
+		ad.show();
+	} // editTextDialog
 } // class Dialog
