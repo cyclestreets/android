c496142ca36a8755e05de975ee502e75c500b3da
Jez Higgins <jez@jezuk.co.uk>
2011-09-09 23:16:45 +0100

    whitespace changes

--- src/net/cyclestreets/RouteActivity.java
+++ src/net/cyclestreets/RouteActivity.java
@@ -29,44 +29,44 @@ public class RouteActivity extends Activity
 	private RadioGroup routeTypeGroup;
 	private Button routeGo;
 	
-    @Override
-    public void onCreate(Bundle saved)
-    {
-        super.onCreate(saved);
+	@Override
+	public void onCreate(Bundle saved)
+	{
+	  super.onCreate(saved);
 
-        setContentView(R.layout.route);
-    	getWindow().setGravity(Gravity.TOP|Gravity.FILL_HORIZONTAL);       
-        getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-        getWindow().setBackgroundDrawableResource(R.drawable.empty);
+	  setContentView(R.layout.route);
+	  getWindow().setGravity(Gravity.TOP|Gravity.FILL_HORIZONTAL);       
+	  getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+	  getWindow().setBackgroundDrawableResource(R.drawable.empty);
 	       
-        placeFrom_ = (PlaceView)findViewById(R.id.placeFrom);
-        placeTo_ = (PlaceView)findViewById(R.id.placeTo);
-    	
-    	final BoundingBoxE6 bounds = GeoIntent.getBoundingBox(getIntent());
-    	placeFrom_.setBounds(bounds);
-    	placeTo_.setBounds(bounds);
+	  placeFrom_ = (PlaceView)findViewById(R.id.placeFrom);
+	  placeTo_ = (PlaceView)findViewById(R.id.placeTo);
+	  
+	  final BoundingBoxE6 bounds = GeoIntent.getBoundingBox(getIntent());
+	  placeFrom_.setBounds(bounds);
+	  placeTo_.setBounds(bounds);
 
-    	final Intent intent = getIntent();
-    	final GeoPoint loc = GeoIntent.getGeoPoint(intent);
-    	if(loc != null)
-    	{
-    		placeFrom_.allowCurrentLocation(loc);
-    		placeTo_.requestFocus();
-     	} // if ...
+	  final Intent intent = getIntent();
+	  final GeoPoint loc = GeoIntent.getGeoPoint(intent);
+	  if(loc != null)
+	  {
+	    placeFrom_.allowCurrentLocation(loc);
+	    placeTo_.requestFocus();
+    } // if ...
     	
-    	final GeoPoint start = GeoIntent.getGeoPoint(intent, "START");
-    	final GeoPoint finish = GeoIntent.getGeoPoint(intent, "FINISH");
-    	placeFrom_.allowLocation(start, "Start marker");
-    	placeTo_.allowLocation(finish, "Finish marker");
-    	placeTo_.allowLocation(start, "Start marker");
-    	placeFrom_.allowLocation(finish, "Finish marker");
+	  final GeoPoint start = GeoIntent.getGeoPoint(intent, "START");
+	  final GeoPoint finish = GeoIntent.getGeoPoint(intent, "FINISH");
+	  placeFrom_.allowLocation(start, "Start marker");
+	  placeTo_.allowLocation(finish, "Finish marker");
+	  placeTo_.allowLocation(start, "Start marker");
+	  placeFrom_.allowLocation(finish, "Finish marker");
     	
-    	routeGo = (Button) findViewById(R.id.routeGo);
-    	routeGo.setOnClickListener(this);
-
-    	routeTypeGroup = (RadioGroup) findViewById(R.id.routeTypeGroup);
-    	routeTypeGroup.check(RouteTypeMapper.idFromName(CycleStreetsPreferences.routeType()));  	
-    } // RouteActivity
+	  routeGo = (Button) findViewById(R.id.routeGo);
+	  routeGo.setOnClickListener(this);
+	  
+	  routeTypeGroup = (RadioGroup) findViewById(R.id.routeTypeGroup);
+	  routeTypeGroup.check(RouteTypeMapper.idFromName(CycleStreetsPreferences.routeType()));  	
+  } // RouteActivity
     
 	private void resolvePlaces()
 	{
@@ -87,13 +87,13 @@ public class RouteActivity extends Activity
 		placeTo_.addHistory(to);
 			
 		// return start and finish points to RouteMapActivity and close
-        final Intent intent = new Intent(RouteActivity.this, RouteMapActivity.class);
-        GeoIntent.setGeoPoint(intent, "FROM", from.coord());
-        GeoIntent.setGeoPoint(intent, "TO", to.coord());
-        final String routeType = RouteTypeMapper.nameFromId(routeTypeGroup.getCheckedRadioButtonId());
-        intent.putExtra(CycleStreetsConstants.EXTRA_ROUTE_TYPE, routeType);
-        setResult(RESULT_OK, intent);
-        finish();
+		final Intent intent = new Intent(RouteActivity.this, RouteMapActivity.class);
+		GeoIntent.setGeoPoint(intent, "FROM", from.coord());
+		GeoIntent.setGeoPoint(intent, "TO", to.coord());
+		final String routeType = RouteTypeMapper.nameFromId(routeTypeGroup.getCheckedRadioButtonId());
+		intent.putExtra(CycleStreetsConstants.EXTRA_ROUTE_TYPE, routeType);
+		setResult(RESULT_OK, intent);
+		finish();
 	} // findRoute
 
 	@Override
