4808bdcbad0eb95ddf19b3a0aa30b317089f3a1c
Jez Higgins <jez@jezuk.co.uk>
2011-10-26 09:04:48 +0100

    whitespace changes

--- src/net/cyclestreets/SettingsActivity.java
+++ src/net/cyclestreets/SettingsActivity.java
@@ -9,61 +9,61 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 
 public class SettingsActivity extends PreferenceActivity 
-							  implements SharedPreferences.OnSharedPreferenceChangeListener
-							  
+                implements SharedPreferences.OnSharedPreferenceChangeListener
+                
 {
-	@Override 
-	public void onCreate(final Bundle savedInstanceState) 
-	{ 
-		super.onCreate(savedInstanceState); 
+  @Override 
+  public void onCreate(final Bundle savedInstanceState) 
+  { 
+    super.onCreate(savedInstanceState); 
 
-		addPreferencesFromResource(R.xml.preferences);
-        setSummary(CycleStreetsPreferences.PREF_ROUTE_TYPE_KEY);
-        setSummary(CycleStreetsPreferences.PREF_UNITS_KEY);
-        setSummary(CycleStreetsPreferences.PREF_SPEED_KEY);
-        setSummary(CycleStreetsPreferences.PREF_MAPSTYLE_KEY);
-        setSummary(CycleStreetsPreferences.PREF_UPLOAD_SIZE);
-   	} // onCreate
+    addPreferencesFromResource(R.xml.preferences);
+    setSummary(CycleStreetsPreferences.PREF_ROUTE_TYPE_KEY);
+    setSummary(CycleStreetsPreferences.PREF_UNITS_KEY);
+    setSummary(CycleStreetsPreferences.PREF_SPEED_KEY);
+    setSummary(CycleStreetsPreferences.PREF_MAPSTYLE_KEY);
+    setSummary(CycleStreetsPreferences.PREF_UPLOAD_SIZE);
+  } // onCreate
 
-    @Override
-    protected void onResume() 
-    {
-        super.onResume();
+  @Override
+  protected void onResume() 
+  {
+    super.onResume();
 
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         
-		final PreferenceScreen account = (PreferenceScreen)findPreference(CycleStreetsPreferences.PREF_ACCOUNT_KEY);
-		if(CycleStreetsPreferences.accountOK())
-			account.setSummary("Signed in to CycleStreets");
-		else if(CycleStreetsPreferences.accountPending())
-			account.setSummary("Account registration awaiting verification");
-		else
-			account.setSummary("");
-    } // onResume
+    final PreferenceScreen account = (PreferenceScreen)findPreference(CycleStreetsPreferences.PREF_ACCOUNT_KEY);
+    if(CycleStreetsPreferences.accountOK())
+      account.setSummary("Signed in to CycleStreets");
+    else if(CycleStreetsPreferences.accountPending())
+      account.setSummary("Account registration awaiting verification");
+    else
+      account.setSummary("");
+  } // onResume
 
-    @Override
-    protected void onPause() 
+  @Override
+  protected void onPause() 
+  {
+    super.onPause();
+
+    // stop listening while paused
+    getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);    
+  } // onPause
+    
+  public void onSharedPreferenceChanged(final SharedPreferences prefs, final String key) 
+  {
+    setSummary(key);
+  } // onSharedPreferencesChanged
+    
+  private void setSummary(final String key) 
+  {
+    final Preference prefUI = findPreference(key);
+    if (prefUI instanceof ListPreference) 
+      prefUI.setSummary(((ListPreference)prefUI).getEntry());
+    if (prefUI instanceof EditTextPreference)
     {
-        super.onPause();
-
-        // stop listening while paused
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);    
-    } // onPause
-    
-    public void onSharedPreferenceChanged(final SharedPreferences prefs, final String key) 
-	{
-		setSummary(key);
-	} // onSharedPreferencesChanged
-    
-	private void setSummary(final String key) 
-	{
-		final Preference prefUI = findPreference(key);
-	    if (prefUI instanceof ListPreference) 
-			prefUI.setSummary(((ListPreference)prefUI).getEntry());
-	    if (prefUI instanceof EditTextPreference)
-	    {
-	    	final String t = ((EditTextPreference)prefUI).getText();
-	    	prefUI.setSummary(t);
-	    }
-    } // setSummary
+      final String t = ((EditTextPreference)prefUI).getText();
+      prefUI.setSummary(t);
+    }
+  } // setSummary
 } // class SettingsActivity
