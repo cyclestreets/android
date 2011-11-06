a123a087f1752c7e44468e889dcbf92abbd5922f
Jez Higgins <jez@jezuk.co.uk>
2011-11-06 12:39:02 +0000

    Added PhotomapCategory - omitted last time

---
+++ src/net/cyclestreets/api/PhotomapCategory.java
@@ -0,0 +1,23 @@
+package net.cyclestreets.api;
+
+public class PhotomapCategory
+{
+	private String tag_, name_, description_;
+
+	PhotomapCategory(final String tag, 
+        	         final String name,
+        	         final String description,
+        	         final long ordering)
+	{
+	  tag_ = tag;
+	  name_ = name;
+	  description_ = description;
+	} // PhotomapCategory
+	
+	public String getName() { return name_; }
+	public String getTag() { return tag_; }
+	public String getDescription() { return description_; }
+	
+	@Override
+	public String toString() { return tag_; }
+} // class PhotomapCategory
