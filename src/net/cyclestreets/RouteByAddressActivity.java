383afcfbd267938406bf33496aaefa863ec70d82
Jez Higgins <jez@jezuk.co.uk>
2012-02-01 09:59:06 +0000

    removed unused import

--- src/net/cyclestreets/RouteByAddressActivity.java
+++ src/net/cyclestreets/RouteByAddressActivity.java
@@ -19,7 +19,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
