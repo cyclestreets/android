fc94ff58e2f6933dd72ef77138c537369b7e5131
Jez Higgins <jez@jezuk.co.uk>
2012-06-20 20:15:44 +0100

    removed redundant import

--- src/net/cyclestreets/CycleStreetsPreferences.java
+++ src/net/cyclestreets/CycleStreetsPreferences.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 
