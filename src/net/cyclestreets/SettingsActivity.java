d50e623a3e5b861abb219c99946b3bc5ffefe8ba
Jez Higgins <jez@jezuk.co.uk>
2012-06-21 21:40:09 +0100

    Factored out map pack loading

--- src/net/cyclestreets/SettingsActivity.java
+++ src/net/cyclestreets/SettingsActivity.java
@@ -1,18 +1,13 @@
 package net.cyclestreets;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import net.cyclestreets.api.POICategories;
+import net.cyclestreets.util.MapPack;
 import net.cyclestreets.util.MessageBox;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Environment;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -41,14 +36,6 @@ public class SettingsActivity extends PreferenceActivity
     setSummary(CycleStreetsPreferences.PREF_UPLOAD_SIZE);
   } // onCreate
   
-  private class CycleStreetsMapFilter implements FilenameFilter
-  {
-    public boolean accept(final File dir, final String name)
-    {
-      return name.contains("net.cyclestreets.maps");
-    } // accept
-  } // class CycleStreetsMapFilter
-  
   private void setupMapFileList()
   {
     final ListPreference mapfilePref= (ListPreference)findPreference(CycleStreetsPreferences.PREF_MAPFILE_KEY);
@@ -57,47 +44,19 @@ public class SettingsActivity extends PreferenceActivity
   
   private void populateMapFileList(final ListPreference mapfilePref)
   {
-    final File obbDir = new File(Environment.getExternalStorageDirectory(), "Android/obb");
-    if(!obbDir.exists())
-      return;
-    
     final List<String> names = new ArrayList<String>();
     final List<String> files = new ArrayList<String>();
-    for(final File mapDir : obbDir.listFiles(new CycleStreetsMapFilter()))
+
+    for(final MapPack pack : MapPack.availableMapPacks())
     {
-      final File map = findMapFile(mapDir, "main.");
-      final String name = mapName(mapDir);
-      if(map == null || name == null)
-        continue;
-      
-      names.add(name);
-      files.add(map.getAbsolutePath());      
+      names.add(pack.name);
+      files.add(pack.path);      
     } // for
     
     mapfilePref.setEntries(names.toArray(new String[] { }));
     mapfilePref.setEntryValues(files.toArray(new String[] { }));
   } // populateMapFileList
   
-  private File findMapFile(final File mapDir, final String prefix)
-  {
-    for(final File c : mapDir.listFiles())
-      if(c.getName().startsWith(prefix))
-        return c;
-    return null;
-  } // findMapFile
-  
-  private String mapName(final File mapDir)
-  {
-    try {
-      final File detailsFile = findMapFile(mapDir, "patch.");
-      final Properties details = new Properties();
-      details.load(new FileInputStream(detailsFile));
-      return details.getProperty("title");
-    } // try
-    catch(IOException e) {
-      return null;
-    }
-  } // mapName
 
   @Override
   protected void onResume() 
---
+++ src/net/cyclestreets/util/MapPack.java
@@ -0,0 +1,73 @@
+package net.cyclestreets.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import android.os.Environment;
+
+public class MapPack
+{
+  final public String name;
+  final public String path;
+
+  static public List<MapPack> availableMapPacks()
+  {
+    final List<MapPack> packs = new ArrayList<MapPack>();
+    
+    final File obbDir = new File(Environment.getExternalStorageDirectory(), "Android/obb");
+    if(!obbDir.exists())
+      return packs;
+    
+    for(final File mapDir : obbDir.listFiles(new CycleStreetsMapFilter()))
+    {
+      final File map = findMapFile(mapDir, "main.");
+      final String name = mapName(mapDir);
+      if(map == null || name == null)
+        continue;
+
+      packs.add(new MapPack(name, map));
+    } // for
+    
+    return packs;
+  } // availableMapPacks
+
+  private MapPack(final String n, final File p) 
+  { 
+    name = n;
+    path = p.getAbsolutePath();
+  } // MapPack
+
+  static private File findMapFile(final File mapDir, final String prefix)
+  {
+    for(final File c : mapDir.listFiles())
+      if(c.getName().startsWith(prefix))
+        return c;
+    return null;
+  } // findMapFile
+  
+  static private String mapName(final File mapDir)
+  {
+    try {
+      final File detailsFile = findMapFile(mapDir, "patch.");
+      final Properties details = new Properties();
+      details.load(new FileInputStream(detailsFile));
+      return details.getProperty("title");
+    } // try
+    catch(IOException e) {
+      return null;
+    } // catch
+  } // mapName
+  
+  static private class CycleStreetsMapFilter implements FilenameFilter
+  {
+    public boolean accept(final File dir, final String name)
+    {
+      return name.contains("net.cyclestreets.maps");
+    } // accept
+  } // class CycleStreetsMapFilter
+} // class MapPack
