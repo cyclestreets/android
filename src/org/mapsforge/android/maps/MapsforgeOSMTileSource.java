d36ff5be62eece44ffd756c92b1d576f2b39c187
Jez Higgins <jez@jezuk.co.uk>
2012-06-27 19:53:32 +0100

    Don't try to draw a tile if we've moved off the edge of the map

--- src/org/mapsforge/android/maps/MapsforgeOSMTileSource.java
+++ src/org/mapsforge/android/maps/MapsforgeOSMTileSource.java
@@ -6,6 +6,7 @@ import org.mapsforge.android.maps.mapgenerator.JobParameters;
 import org.mapsforge.android.maps.mapgenerator.JobTheme;
 import org.mapsforge.android.maps.mapgenerator.MapGeneratorJob;
 import org.mapsforge.android.maps.mapgenerator.databaserenderer.DatabaseRenderer;
+import org.mapsforge.core.BoundingBox;
 import org.mapsforge.core.Tile;
 import org.mapsforge.map.reader.MapDatabase;
 import org.osmdroid.ResourceProxy;
@@ -45,6 +46,12 @@ public class MapsforgeOSMTileSource implements ITileSource
   private final JobParameters jobParameters_;
   private final DebugSettings debugSettings_;
   private String mapFile_;
+  private BoundingBox mapBounds_;
+  private int zoomBounds_;
+  private int westTileBounds_;
+  private int eastTileBounds_;
+  private int southTileBounds_;
+  private int northTileBounds_;
   
   public MapsforgeOSMTileSource(final String name)
   {
@@ -52,7 +59,7 @@ public class MapsforgeOSMTileSource implements ITileSource
     mapGenerator_ = new DatabaseRenderer();
     mapDatabase_ = new MapDatabase();
     mapGenerator_.setMapDatabase(mapDatabase_);
-    
+        
     jobParameters_ = new JobParameters(new RenderTheme(), DEFAULT_TEXT_SCALE);
     debugSettings_ = new DebugSettings(false, false, false);
   } // MapsforgeOSMTileSource
@@ -65,6 +72,8 @@ public class MapsforgeOSMTileSource implements ITileSource
     mapFile_ = mapFile;
     mapDatabase_.closeFile();
     mapDatabase_.openFile(mapFile_);
+    mapBounds_ = mapDatabase_.getMapFileInfo().boundingBox;
+    zoomBounds_ = -1;
   } // setMapFile
 
   @Override
@@ -81,7 +90,10 @@ public class MapsforgeOSMTileSource implements ITileSource
   public int getMinimumZoomLevel() { return 6; }
 
   public synchronized Drawable getDrawable(int tileX, int tileY, int zoom) throws LowMemoryException
-  {
+  {	
+	if(tileOutOfBounds(tileX, tileY, zoom))
+		return null;
+	  
     final Tile tile = new Tile(tileX, tileY, (byte)zoom);
     MapGeneratorJob mapGeneratorJob = new MapGeneratorJob(tile, 
                                                           "ooot",                                                           
@@ -92,6 +104,26 @@ public class MapsforgeOSMTileSource implements ITileSource
     boolean success = mapGenerator_.executeJob(mapGeneratorJob, tileBitmap);
     return success ?  new ExpirableBitmapDrawable(tileBitmap) : null;
   } // getDrawable
+  
+  private boolean tileOutOfBounds(int tileX, int tileY, int zoom)
+  {
+    if(zoom != zoomBounds_)
+      recalculateTileBounds(zoom);
+			
+    final boolean oob =  (tileX < westTileBounds_) || (tileX > eastTileBounds_) ||
+    	                 (tileY < northTileBounds_) || (tileY > southTileBounds_);
+    return oob;
+  } // tileOutOfBounds
+  
+  /* convert lon/lat to tile x,y from http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames */
+  private void recalculateTileBounds(final int zoom)
+  {
+    zoomBounds_ = zoom;
+    westTileBounds_ = lon2XTile(mapBounds_.getMinLongitude(), zoomBounds_);
+    eastTileBounds_ = lon2XTile(mapBounds_.getMaxLongitude(), zoomBounds_);
+    southTileBounds_ = lat2YTile(mapBounds_.getMinLatitude(), zoomBounds_);
+    northTileBounds_ = lat2YTile(mapBounds_.getMaxLatitude(), zoomBounds_);
+  } // recalculateTileBounds
   
   @Override
   public Drawable getDrawable(String arg0) throws LowMemoryException
@@ -110,4 +142,14 @@ public class MapsforgeOSMTileSource implements ITileSource
   {
     return null;
   } // getTileRelativeFilenameString
+  
+  static private int lon2XTile(final double lon, final int zoom)
+  {
+	return (int)Math.floor((lon + 180) / 360 * (1<<zoom)) ;
+  } // lon2XTile
+  
+  static private int lat2YTile(final double lat, final int zoom)
+  {
+	return (int)Math.floor((1 - Math.log(Math.tan(Math.toRadians(lat)) + 1 / Math.cos(Math.toRadians(lat))) / Math.PI) / 2 * (1<<zoom)) ;	  
+  } // lat2YTile
 } // MapsforgeOSMTileSource
