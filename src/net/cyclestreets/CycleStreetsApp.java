89e92034cf81d619ba3e4b9692208d9f37a3bb4f
Jez Higgins <jez@jezuk.co.uk>
2012-09-18 06:24:30 +0100

    Pulled out RegularUpdates into its own class

--- src/net/cyclestreets/CycleStreetsApp.java
+++ src/net/cyclestreets/CycleStreetsApp.java
@@ -1,18 +1,9 @@
 package net.cyclestreets;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 import net.cyclestreets.api.ApiClient;
-import net.cyclestreets.api.Blog;
 import net.cyclestreets.planned.Route;
 import android.app.Application;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import org.acra.ACRA;
@@ -33,7 +24,6 @@ import org.acra.annotation.ReportsCrashes;
                 )
 public class CycleStreetsApp extends Application 
 {
-  private static Timer updater_;
   private final static int oneMinute = 1000*60;
   private final static int aDay = 1000*60*60*24; // in milliseconds
   
@@ -47,9 +37,8 @@ public class CycleStreetsApp extends Application
 	    
 	  Route.initialise(this);
 	  ApiClient.initialise(this);
-	      
-    updater_ = new Timer();
-    updater_.schedule(new RegularUpdates(this), oneMinute, aDay);
+	 
+	  RegularUpdates.schedule(this, oneMinute, aDay);
 	} // onCreate
 	
 	public String version()
@@ -69,68 +58,4 @@ public class CycleStreetsApp extends Application
     } // catch
   } // versionName
   
-  static private class RegularUpdates extends TimerTask
-  {
-    private final Context context_;
-    
-    RegularUpdates(final Context context)
-    {
-      context_ = context;
-    } // RegularUpdates
-    
-    public void run() 
-    {
-      final Blog blog = Blog.load();
-    
-      // check for new blog entries
-      if(blog == null)
-        return;
-      
-      if(blog.mostRecent().equals(lastBlogUpdate()))
-        return;
-      
-      setLastBlogUpdate(blog.mostRecent());
-
-      notify(s(R.string.blog_cyclestreets_update), blog.mostRecentTitle(), s(R.string.blog_update), BlogActivity.class);
-    } // run
-    
-    private <T> void notify(final String title, final String text, final String ticker, final Class<T> tap)
-    {
-      final NotificationManager nm = (NotificationManager)context_.getSystemService(Context.NOTIFICATION_SERVICE);
-      final Notification notification = new Notification(R.drawable.icon, ticker, System.currentTimeMillis());
-      notification.flags = Notification.FLAG_AUTO_CANCEL;
-      final Intent notificationIntent = new Intent(context_, tap);
-      final PendingIntent contentIntent = 
-           PendingIntent.getActivity(context_, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-      notification.setLatestEventInfo(context_, title, text, contentIntent);
-      nm.notify(1, notification);
-    } // notify
-    
-    private String s(final int id)
-    {
-      return context_.getResources().getString(id);
-    } // s
-    
-    private final String LAST_DATE = "lastDate";
-    
-    private String lastBlogUpdate()
-    {
-      final SharedPreferences prefs = prefs();
-      return prefs.getString(LAST_DATE, null);
-    } // lastBlogUpdate
-    
-    private void setLastBlogUpdate(final String update)
-    {
-      final SharedPreferences.Editor edit = prefs().edit();
-      edit.putString(LAST_DATE, update);
-      edit.commit();
-    } // setLastBlogUpdate
-    
-    private SharedPreferences prefs()
-    {
-      return context_.getSharedPreferences("net.cyclestreets.app", Context.MODE_PRIVATE);
-    } // prefs
-  } // RegularUpdates
-
-
 } // CycleStreetsApp
