2373caedb7990865fc5ab0d19e501ec60eab6818
Jez Higgins <jez@jezuk.co.uk>
2012-09-27 18:08:09 +0100

    Make the service responsible for knowing if it's active

--- src/net/cyclestreets/service/LiveRideService.java
+++ src/net/cyclestreets/service/LiveRideService.java
@@ -1,23 +1,45 @@
 package net.cyclestreets.service;
 
+import net.cyclestreets.CycleStreets;
+import net.cyclestreets.R;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 
-public class LiveRideService extends Service
+public class LiveRideService extends Service implements LocationListener
 {
-  private final IBinder binder_ = new LocalBinder();
+  private IBinder binder_;
+  private LocationManager locationManager_;
+  private boolean riding_;
+
+  @Override
+  public void onCreate() 
+  {
+    binder_ = new Binding();
+    locationManager_ = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    notify("CycleStreets", "Here we go!", "We're off!");
+  } // onCreate
   
   @Override
   public int onStartCommand(final Intent intent, final int flags, final int startId) 
-  {
+  {    
     return Service.START_NOT_STICKY;
   } // onStartCommand
   
   @Override 
   public void onDestroy() 
   {
+    stopRiding();
+    cancelNotification();
     super.onDestroy();
   } // onDestroy
   
@@ -26,13 +48,81 @@ public class LiveRideService extends Service
   {
     return binder_;
   } // onBind
-
-  public class LocalBinder extends Binder 
+  
+  public void startRiding() 
   {
-    public LiveRideService getService() 
+    if(riding_)
+      return;
+    
+    locationManager_.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    riding_ = true;
+  } // startRiding
+  
+  public void stopRiding() 
+  {
+    if(!riding_)
+      return;
+    
+    locationManager_.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    riding_ = false;
+  } // stopRiding
+  
+  public boolean areRiding()
+  {
+    return riding_;
+  } // onRide
+  
+  private <T> void notify(final String title, final String text, final String ticker)
+  {
+    final NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+    final Notification notification = new Notification(R.drawable.icon, ticker, System.currentTimeMillis());
+    notification.flags = Notification.FLAG_AUTO_CANCEL | Notification.FLAG_ONGOING_EVENT;
+    final Intent notificationIntent = new Intent(this, CycleStreets.class);
+    final PendingIntent contentIntent = 
+         PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+    notification.setLatestEventInfo(this, title, text, contentIntent);
+    nm.notify(1, notification);
+  } // notify
+  
+  void cancelNotification()
+  {
+    final NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+    nm.cancel(1);
+  } // cancelNotification
+  
+  public class Binding extends Binder 
+  {
+    private LiveRideService service() 
     {
       return LiveRideService.this;
     } // getService
+    
+    public void startRiding() { service().startRiding(); }
+    public void stopRiding() { service().stopRiding(); }
+    public boolean areRiding() { return service().areRiding(); }
   } // class LocalBinder
+
+  /////////////////////////////////////////////////
+  // location listener
+  @Override
+  public void onLocationChanged(final Location location)
+  {
+    notify("CycleStreets", "We've moved", "Cooking with gas");
+  } // onLocationChanged
+
+  @Override
+  public void onProviderDisabled(final String arg0)
+  {
+  } // onProviderDisabled
+
+  @Override
+  public void onProviderEnabled(String arg0)
+  {
+  } // onProviderEnabled
+
+  @Override
+  public void onStatusChanged(String arg0, int arg1, Bundle arg2)
+  {
+  } // onStatusChanged
   
 } // class LiveRideService
--- src/net/cyclestreets/views/overlay/LiveRideOverlay.java
+++ src/net/cyclestreets/views/overlay/LiveRideOverlay.java
@@ -11,6 +11,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Canvas;
 import android.os.IBinder;
 import android.view.Menu;
@@ -18,17 +20,15 @@ import android.view.MenuItem;
 import static net.cyclestreets.FragmentHelper.createMenuItem;
 import static net.cyclestreets.FragmentHelper.showMenuItem;
 
-public class LiveRideOverlay extends Overlay implements MenuListener, ServiceConnection
+public class LiveRideOverlay extends Overlay implements MenuListener, PauseResumeListener, ServiceConnection
 {
   private Context context_;
-  private boolean active_;
-  private LiveRideService.LocalBinder binding_;
+  private LiveRideService.Binding binding_;
   
   public LiveRideOverlay(final Context context)
   {
     super(context);
     context_ = context;
-    active_ = false;
   } // LiveRideOverlay
 
   @Override
@@ -38,17 +38,17 @@ public class LiveRideOverlay extends Overlay implements MenuListener, ServiceCon
   } // draw
 
   @Override
-  public void onCreateOptionsMenu(Menu menu)
+  public void onCreateOptionsMenu(final Menu menu)
   {
     createMenuItem(menu, R.string.ic_menu_liveride, Menu.NONE, 0);
     createMenuItem(menu, R.string.ic_menu_stopride, Menu.NONE, 0);
   } // onCreateOptionsMenu
 
   @Override
-  public void onPrepareOptionsMenu(Menu menu)
+  public void onPrepareOptionsMenu(final Menu menu)
   {
-    showMenuItem(menu, R.string.ic_menu_liveride, Route.available() && !active_);
-    showMenuItem(menu, R.string.ic_menu_stopride, Route.available() && active_);
+    showMenuItem(menu, R.string.ic_menu_liveride, Route.available() && !areRiding());
+    showMenuItem(menu, R.string.ic_menu_stopride, Route.available() && areRiding());
   } // onPrepareOptionsMenu
 
   @Override
@@ -67,28 +67,48 @@ public class LiveRideOverlay extends Overlay implements MenuListener, ServiceCon
     } // switch
   } // onMenuItemSelected
   
-  private void enableLiveRide() 
+  @Override
+  public void onPause(final Editor prefs)
+  {
+    context_.unbindService(this);
+  } // onPause
+
+  @Override
+  public void onResume(final SharedPreferences prefs)
   {
     final Intent intent = new Intent(context_, LiveRideService.class);
     context_.bindService(intent, this, Context.BIND_AUTO_CREATE);    
+  } // onResume
+  
+  private boolean areRiding() 
+  {
+    return binding_ != null && binding_.areRiding();
+  } // areRiding
+
+  private void enableLiveRide() 
+  {
+    if(binding_ == null)
+      return;
+    
+    binding_.startRiding();
   } // enableLiveRide
   
   private void disableLiveRide() 
   {
-    active_ = false;
-    context_.unbindService(this);
+    if(binding_ == null)
+      return;
+    
+    binding_.stopRiding();
   } // disableLiveRide
 
   @Override
   public void onServiceConnected(final ComponentName className, final IBinder binder)
   {
-    active_ = true;
-    binding_ = (LiveRideService.LocalBinder)binder;
+    binding_ = (LiveRideService.Binding)binder;
   } // onServiceConnected
 
   @Override
   public void onServiceDisconnected(final ComponentName className)
   {
-    active_ = false;
   } // onServiceDisconnected
 } // LiveRideOverlay
