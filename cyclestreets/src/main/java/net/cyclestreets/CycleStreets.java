d419b21e534e20800360ef6fcb53a878bc6b4568
Jez Higgins <jez@jezuk.co.uk>
2014-04-28 15:43:43 +0100

    Register for http://cycle.st/jNNNNNNNN urls as per issue #86

--- cyclestreets/src/main/java/net/cyclestreets/CycleStreets.java
+++ cyclestreets/src/main/java/net/cyclestreets/CycleStreets.java
@@ -1,7 +1,10 @@
 package net.cyclestreets;
 
+import net.cyclestreets.routing.Route;
 import net.cyclestreets.util.MapPack;
 
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TabHost;
 
@@ -10,7 +13,32 @@ public class CycleStreets extends MainTabbedActivity
 	public void onCreate(final Bundle savedInstanceState)	{
     switchMapFile();
     super.onCreate(savedInstanceState);
-	} // onCreate
+
+    final Uri launchUri = getIntent().getData();
+    if (launchUri == null)
+      return;
+
+    final int itinerary = extractItinerary(launchUri);
+    if (itinerary == -1)
+      return;
+
+    Route.FetchRoute(CycleStreetsPreferences.routeType(),
+        itinerary,
+        CycleStreetsPreferences.speed(),
+        this);
+    final SharedPreferences.Editor edit = prefs().edit();
+    edit.putString("TAB", "");
+    edit.commit();
+  } // onCreate
+
+  private int extractItinerary(final Uri launchUri) {
+    try {
+      final String path = launchUri.getPath();
+      return Integer.parseInt(path.substring(2));
+    } catch(Exception whatever) {
+      return -1;
+    } // catch
+  } // extractItinerary
 
   protected void addTabs(final TabHost tabHost) {
     addTab("Route Map", R.drawable.ic_tab_planroute, RouteMapFragment.class);
