17ac8f22632cd64e333135597a05fabef33118eb
Jez Higgins <jez@jezuk.co.uk>
2014-09-29 23:04:50 +0100

    Renamed v2 to vNext

--- cyclestreets.vNext/src/main/java/net/cyclestreets/v2/CycleStreets.java
+++ cyclestreets.vNext/src/main/java/net/cyclestreets/v2/CycleStreets.java
@@ -1,12 +1,23 @@
 package net.cyclestreets.v2;
 
+import android.os.Bundle;
+
 import net.cyclestreets.ItineraryFragment;
 import net.cyclestreets.MainNavDrawerActivity;
+import net.cyclestreets.MainSupport;
 import net.cyclestreets.MoreFragment;
 import net.cyclestreets.PhotoMapFragment;
 import net.cyclestreets.RouteMapFragment;
 
 public class CycleStreets extends MainNavDrawerActivity {
+  public void onCreate(final Bundle savedInstanceState)	{
+    MainSupport.switchMapFile(getIntent());
+
+    super.onCreate(savedInstanceState);
+
+    MainSupport.loadRoute(getIntent(), this);
+  } // onCreate
+
   @Override
   protected void addPages() {
     addPage("Route Map", RouteMapFragment.class);
