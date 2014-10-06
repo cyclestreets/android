fdf5e55d4a464478ac9460ffb35c79a624231ec8
Jez Higgins <jez@jezuk.co.uk>
2014-10-06 20:42:29 +0100

    Added Blog page

--- cyclestreets.vNext/src/main/java/net/cyclestreets/v2/CycleStreets.java
+++ cyclestreets.vNext/src/main/java/net/cyclestreets/v2/CycleStreets.java
@@ -9,6 +9,7 @@ import net.cyclestreets.MoreFragment;
 import net.cyclestreets.PhotoMapFragment;
 import net.cyclestreets.PhotoUploadFragment;
 import net.cyclestreets.RouteMapFragment;
+import net.cyclestreets.WebPageFragment;
 
 public class CycleStreets extends MainNavDrawerActivity {
   public void onCreate(final Bundle savedInstanceState)	{
@@ -25,6 +26,11 @@ public class CycleStreets extends MainNavDrawerActivity {
     addPage("Itinerary", R.drawable.ic_menu_agenda_white, ItineraryFragment.class);
     addPage("Photomap", R.drawable.ic_menu_gallery_white, PhotoMapFragment.class);
     addPage("Photo Upload", R.drawable.ic_menu_camera_white, PhotoUploadFragment.class);
+    addPage("Blog",
+        -1,
+        WebPageFragment.class,
+        WebPageFragment.initialiser("http://cyclestreets.net/blog/"));
+
     addPage("More ...", R.drawable.ic_menu_info_details_white, MoreFragment.class);
   } // addPages
 } // CycleStreets
