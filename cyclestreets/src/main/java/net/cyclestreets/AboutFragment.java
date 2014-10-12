7faf4edf5aa62cc371a1bc123f9e6567e47700d8
Jez Higgins <jez@jezuk.co.uk>
2014-10-12 17:10:08 +0100

    Moved About out of library

--- cyclestreets/src/main/java/net/cyclestreets/AboutFragment.java
+++ cyclestreets/src/main/java/net/cyclestreets/AboutFragment.java
@@ -8,14 +8,12 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.TextView;
 
-import net.cyclestreets.fragments.R;
-
 public class AboutFragment extends Fragment {
   @Override
   public View onCreateView(final LayoutInflater inflater,
                            final ViewGroup container,
                            final Bundle savedInstanceState) {
-    final View about = inflater.inflate(R.layout.about, null);
+    final View about = inflater.inflate(R.layout.about, container, false);
 
     final WebView htmlView = (WebView)about.findViewById(R.id.html_view);
     htmlView.loadUrl("file:///android_asset/credits.html");
