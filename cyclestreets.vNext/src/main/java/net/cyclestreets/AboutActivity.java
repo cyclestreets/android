7309819a5bdbb9408308f89d0b552475c4102c6b
Jez Higgins <jez@jezuk.co.uk>
2014-10-26 23:08:22 +0000

    Got preferences working properly

---
+++ cyclestreets.vNext/src/main/java/net/cyclestreets/AboutActivity.java
@@ -0,0 +1,7 @@
+package net.cyclestreets;
+
+import android.support.v4.app.Fragment;
+
+public class AboutActivity extends FragmentHolder {
+  protected Fragment fragment() { return new AboutFragment(); }
+} // AboutActivity
---
+++ cyclestreets.vNext/src/main/java/net/cyclestreets/AboutFragment.java
@@ -0,0 +1,34 @@
+package net.cyclestreets;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+public class AboutFragment extends Fragment {
+  @Override
+  public View onCreateView(final LayoutInflater inflater,
+                           final ViewGroup container,
+                           final Bundle savedInstanceState) {
+    final View about = inflater.inflate(R.layout.about, container, false);
+
+    final WebView htmlView = (WebView)about.findViewById(R.id.html_view);
+    htmlView.loadUrl("file:///android_asset/credits.html");
+
+    final TextView versionView = (TextView)about.findViewById(R.id.version_view);
+    versionView.setText(versionName());
+
+    return about;
+  } // onCreateView
+
+  private String versionName() {
+    try {
+      return CycleStreetsAppSupport.version(getActivity());
+    } catch(Exception e) {
+      return "Unknown";
+    }
+  } // versionName
+} // AboutFragment
