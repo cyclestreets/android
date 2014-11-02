62724324902394ae1570c7df24c2bbaa1b9464be
Jez Higgins <jez@jezuk.co.uk>
2014-11-02 17:37:03 +0000

    Can now add a location

---
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/LocationEditorActivity.java
@@ -0,0 +1,13 @@
+package net.cyclestreets;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.cyclestreets.fragments.R;
+
+public class LocationEditorActivity extends Fragment {
+
+} // LocationEditorFragment
