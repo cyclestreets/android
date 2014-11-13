332229267db7e321dd07a9d5fa2550b28ffb384b
Jez Higgins <jez@jezuk.co.uk>
2014-11-13 23:11:03 +0000

    whitespace changes

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/Dialog.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/Dialog.java
@@ -10,12 +10,9 @@ import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
-public class Dialog 
-{
-  private static class CycleStreetsProgressDialog extends ProgressDialog
-  {
-    public CycleStreetsProgressDialog(final Context context, final String message) 
-    {
+public class Dialog {
+  private static class CycleStreetsProgressDialog extends ProgressDialog {
+    public CycleStreetsProgressDialog(final Context context, final String message) {
       super(context);
       setMessage(message);
       setIndeterminate(true);
@@ -23,8 +20,7 @@ public class Dialog
     } // CycleStreetsProgressDialog
     
     @Override
-    public void dismiss() 
-    {
+    public void dismiss() {
       try {
         super.dismiss();
       } // try
@@ -34,58 +30,53 @@ public class Dialog
     } // dismiss
   } // class CycleStreetsProgressDialog
   
-	static public ProgressDialog createProgressDialog(final Context context,
-	                                                  final int messageId)
-	{
-		return createProgressDialog(context, context.getString(messageId));
-	} // createProgressDialog
-	
-	static public ProgressDialog createProgressDialog(final Context context,
-	                                                  final String message)
-	{
-		final ProgressDialog progress = new CycleStreetsProgressDialog(context, message);
-		return progress;
-	} // createProgressDialog
-	
-	public interface UpdatedTextListener
-	{
-		public void updatedText(final String updated);
-	} // interface UpdatedTextListener
-	
-	static public void editTextDialog(final Context context, 
-									                  final String initialText,
-									                  final String buttonText,
-									                  final UpdatedTextListener listener)
-	{
+  static public ProgressDialog createProgressDialog(final Context context,
+                                                    final int messageId) {
+    return createProgressDialog(context, context.getString(messageId));
+  } // createProgressDialog
+  
+  static public ProgressDialog createProgressDialog(final Context context,
+                                                    final String message) {
+    final ProgressDialog progress = new CycleStreetsProgressDialog(context, message);
+    return progress;
+  } // createProgressDialog
+  
+  public interface UpdatedTextListener {
+    public void updatedText(final String updated);
+  } // interface UpdatedTextListener
+  
+  static public void editTextDialog(final Context context, 
+                                    final String initialText,
+                                    final String buttonText,
+                                    final UpdatedTextListener listener) {
     final View layout = View.inflate(context, R.layout.edittextdialog, null);
-	  final EditText textBox = ((EditText)layout.findViewById(R.id.edit_text));
-	  textBox.setText(initialText);
+    final EditText textBox = ((EditText)layout.findViewById(R.id.edit_text));
+    textBox.setText(initialText);
 
-	  final AlertDialog.Builder builder = newBuilder(context);
-	  builder.setPositiveButton(buttonText, new DialogInterface.OnClickListener() {
-	  	@Override
-			public void onClick(DialogInterface dialog, int which) {
-				final String t = textBox.getText().toString().trim();
-				listener.updatedText(t);
-			} // onClick
-		});
-	  builder.setView(layout);
+    final AlertDialog.Builder builder = newBuilder(context);
+    builder.setPositiveButton(buttonText, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        final String t = textBox.getText().toString().trim();
+        listener.updatedText(t);
+      } // onClick
+    });
+    builder.setView(layout);
 
-	  show(builder);
-	} // editTextDialog
-	
-	static public void listViewDialog(final Context context,
-	                                  final ListAdapter adapter,
-	                                  final DialogInterface.OnClickListener yesAction)
-	{
-	  listViewDialog(context, adapter, yesAction, MessageBox.NoAction);
-	} // listViewDialog
+    show(builder);
+  } // editTextDialog
+  
+  static public void listViewDialog(final Context context,
+                                    final ListAdapter adapter,
+                                    final DialogInterface.OnClickListener yesAction) {
+    listViewDialog(context, adapter, yesAction, MessageBox.NoAction);
+  } // listViewDialog
 
-	
-	static public void listViewDialog(final Context context,
-	                                  final ListAdapter adapter,
-	                                  final DialogInterface.OnClickListener yesAction,
-	                                  final DialogInterface.OnClickListener noAction) {
+  
+  static public void listViewDialog(final Context context,
+                                    final ListAdapter adapter,
+                                    final DialogInterface.OnClickListener yesAction,
+                                    final DialogInterface.OnClickListener noAction) {
     listViewDialog(context, -1, adapter, yesAction, noAction);
   } // listViewDialog
 
@@ -112,28 +103,25 @@ public class Dialog
     builder.setView(layout);
     
     show(builder);
-	} // listViewDialog
-	
-	static AlertDialog.Builder newBuilder(final View parent)
-	{
-		return newBuilder(parent.getContext());
-	} // newBuilder
-	
-	static AlertDialog.Builder newBuilder(final Context context)
-	{
-		final AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(applicationName(context));
-		return builder;
-	} // newBuilder
+  } // listViewDialog
+  
+  static AlertDialog.Builder newBuilder(final View parent) {
+    return newBuilder(parent.getContext());
+  } // newBuilder
+  
+  static AlertDialog.Builder newBuilder(final Context context) {
+    final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    builder.setTitle(applicationName(context));
+    return builder;
+  } // newBuilder
 
   private static String applicationName(final Context context) {
     int stringId = context.getApplicationInfo().labelRes;
     return context.getString(stringId);
   } // applicationName
 
-  static void show(final AlertDialog.Builder builder)
-	{
-		final AlertDialog ad = builder.create();
-		ad.show();
-	} // show
+  static void show(final AlertDialog.Builder builder) {
+    final AlertDialog ad = builder.create();
+    ad.show();
+  } // show
 } // class Dialog
