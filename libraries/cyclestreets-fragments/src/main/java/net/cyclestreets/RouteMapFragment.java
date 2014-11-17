a615e7f19ebbefeb53928bcb28a365da5a5f9aae
Jez Higgins <jez@jezuk.co.uk>
2014-11-17 12:39:53 +0000

    Factored out StoredRoutes to its own class

--- libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/RouteMapFragment.java
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/RouteMapFragment.java
@@ -1,9 +1,6 @@
 package net.cyclestreets;
 
-import net.cyclestreets.content.RouteSummary;
 import net.cyclestreets.fragments.R;
-import net.cyclestreets.routing.Segment;
-import net.cyclestreets.util.Dialog;
 import net.cyclestreets.util.GPS;
 import net.cyclestreets.util.MessageBox;
 import net.cyclestreets.util.GeoIntent;
@@ -16,25 +13,16 @@ import net.cyclestreets.routing.Route;
 import net.cyclestreets.routing.Waypoints;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static net.cyclestreets.util.MenuHelper.createMenuItem;
 import static net.cyclestreets.util.MenuHelper.enableMenuItem;
@@ -124,7 +112,7 @@ public class RouteMapFragment extends CycleMapFragment
       return true;
     }
     if(R.id.ic_menu_saved_routes == menuId) {
-      launchStoredRoutesDialog();
+      launchStoredRoutes();
       return true;
     }
     if(R.id.ic_menu_route_number == menuId) {
@@ -150,9 +138,9 @@ public class RouteMapFragment extends CycleMapFragment
 			final int speed = data.getIntExtra(CycleStreetsConstants.EXTRA_ROUTE_SPEED,
 					                               CycleStreetsPreferences.speed());
 			Route.PlotRoute(routeType,
-			                speed,
-			                getActivity(),
-                      points);
+          speed,
+          getActivity(),
+          points);
 		} // if ...
 	} // onActivityResult
 
@@ -194,16 +182,9 @@ public class RouteMapFragment extends CycleMapFragment
     RouteNumberActivity.launch(getActivity());
 	} // doLaunchFetchRouteDialog
 
-	private void launchStoredRoutesDialog()
-	{
-    RouteSummaryAdapter rsa = new RouteSummaryAdapter(getActivity());
-    AlertDialog ad = Dialog.listViewDialog(getActivity(),
-                                           R.string.ic_menu_saved_routes,
-                                           rsa,
-                                           null,
-                                           null);
-    rsa.setDialog(ad);
-	} // launchStoredRoutesDialog
+	private void launchStoredRoutes()	{
+    StoredRoutes.launch(getActivity());
+	} // launchStoredRoutes
 
 	private void startNewRoute(final DialogInterface.OnClickListener listener)
 	{
@@ -228,140 +209,4 @@ public class RouteMapFragment extends CycleMapFragment
 	{
     mapView().invalidate();
 	} // onReset
-
-  //////////////////////////////////////////////
-  //////////////////////////////////////////////
-  //////////////////////////////////
-  private static class RouteSummaryAdapter extends BaseAdapter
-                                   implements View.OnClickListener,
-                                              View.OnLongClickListener,
-                                              View.OnCreateContextMenuListener {
-    private final Context context_;
-    private final LayoutInflater inflater_;
-    private List<RouteSummary> routes_;
-    private final Map<View, Integer> viewRoute_;
-    private AlertDialog ad_;
-
-    RouteSummaryAdapter(final Context context) {
-      context_ = context;
-      inflater_ = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      routes_ = Route.storedRoutes();
-      viewRoute_ = new HashMap<>();
-    } // SegmentAdaptor
-
-    public void setDialog(final AlertDialog ad) { ad_ = ad; }
-
-    private void refresh() {
-      routes_ = Route.storedRoutes();
-      notifyDataSetChanged();
-      if (routes_.size() == 0)
-        closeDialog();
-    } // refresh
-
-    private void closeDialog() {
-      if (ad_ != null)
-        ad_.cancel();
-    } // closeDialog
-
-    public RouteSummary getRouteSummary(int localId) {
-      for(final RouteSummary r : routes_)
-        if(r.localId() == localId)
-          return r;
-      return null;
-    } // getRouteSummary
-
-    @Override
-    public int getCount() { return routes_.size(); }
-
-    @Override
-    public Object getItem(int position) { return routes_.get(position); }
-
-    @Override
-    public long getItemId(int position) { return routes_.get(position).localId(); }
-
-    @Override
-    public View getView(final int position, final View convertView, final ViewGroup parent) {
-      final RouteSummary summary = routes_.get(position);
-      final View v = inflater_.inflate(R.layout.storedroutes_item, parent, false);
-      viewRoute_.put(v, summary.localId());
-
-      final TextView n = (TextView)v.findViewById(R.id.route_title);
-
-      final String p = summary.plan();
-      final String plan = p.substring(0,1).toUpperCase() + p.substring(1);
-
-      n.setText(summary.title() + "\n" +
-          plan + " route, " +
-          Segment.formatter.total_distance(summary.distance()));
-
-      v.setOnClickListener(this);
-      v.setOnLongClickListener(this);
-      v.setOnCreateContextMenuListener(this);
-
-      return v;
-    } // getView
-
-    @Override
-    public void onClick(final View view) {
-      final int localId = viewRoute_.get(view);
-      openRoute(localId);
-    } // onClick
-
-    @Override
-    public boolean onLongClick(final View view) {
-      view.showContextMenu();
-      return true;
-    } // onClick
-
-    @Override
-    public void onCreateContextMenu(final ContextMenu menu,
-                                    final View view,
-                                    final ContextMenu.ContextMenuInfo contextMenuInfo) {
-      final MenuItem.OnMenuItemClickListener listener = new MenuItem.OnMenuItemClickListener() {
-        public boolean onMenuItemClick(final MenuItem item) {
-          RouteSummaryAdapter.this.onViewMenuClick(view, item);
-          return true;
-        } // onMenuItemClick
-      };
-      createMenuItem(menu, R.string.ic_menu_open).setOnMenuItemClickListener(listener);
-      createMenuItem(menu, R.string.ic_menu_rename).setOnMenuItemClickListener(listener);
-      createMenuItem(menu, R.string.ic_menu_delete).setOnMenuItemClickListener(listener);
-    } // onCreateContextMenu
-
-    private void onViewMenuClick(final View view, final MenuItem item) {
-      final int localId = viewRoute_.get(view);
-      final int menuId = item.getItemId();
-
-      if(R.string.ic_menu_open == menuId)
-        openRoute(localId);
-      if(R.string.ic_menu_rename == menuId)
-        renameRoute(localId);
-      if(R.string.ic_menu_delete == menuId)
-        deleteRoute(localId);
-    } // onMenuItemClick
-
-    /////////////////////////////////////////////
-    private void openRoute(final int localId) {
-      Route.PlotStoredRoute(localId, context_);
-      closeDialog();
-    } // routeSelected
-
-    private void renameRoute(final int localId)
-    {
-      final RouteSummary route = getRouteSummary(localId);
-      Dialog.editTextDialog(context_, route.title(), "Rename",
-          new Dialog.UpdatedTextListener() {
-            @Override
-            public void updatedText(final String updated) {
-              Route.RenameRoute(localId, updated);
-              refresh();
-            } // updatedText
-          });
-    } // renameRoute
-
-    private void deleteRoute(final int localId) {
-      Route.DeleteRoute(localId);
-      refresh();
-    } // deleteRoute
-  } // class RouteSummaryAdaptor
 } // class MapActivity
---
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/StoredRoutes.java
@@ -0,0 +1,172 @@
+package net.cyclestreets;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import net.cyclestreets.content.RouteSummary;
+import net.cyclestreets.fragments.R;
+import net.cyclestreets.routing.Route;
+import net.cyclestreets.routing.Segment;
+import net.cyclestreets.util.Dialog;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static net.cyclestreets.util.MenuHelper.createMenuItem;
+
+public class StoredRoutes {
+  public static void launch(final Context context) {
+    RouteSummaryAdapter rsa = new RouteSummaryAdapter(context);
+    AlertDialog ad = Dialog.listViewDialog(context,
+        R.string.ic_menu_saved_routes,
+        rsa,
+        null,
+        null);
+    rsa.setDialog(ad);
+  } // launch
+
+  //////////////////////////////////
+  private static class RouteSummaryAdapter extends BaseAdapter
+      implements View.OnClickListener,
+      View.OnLongClickListener,
+      View.OnCreateContextMenuListener {
+    private final Context context_;
+    private final LayoutInflater inflater_;
+    private List<RouteSummary> routes_;
+    private final Map<View, Integer> viewRoute_;
+    private AlertDialog ad_;
+
+    RouteSummaryAdapter(final Context context) {
+      context_ = context;
+      inflater_ = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+      routes_ = Route.storedRoutes();
+      viewRoute_ = new HashMap<>();
+    } // SegmentAdaptor
+
+    public void setDialog(final AlertDialog ad) { ad_ = ad; }
+
+    private void refresh() {
+      routes_ = Route.storedRoutes();
+      notifyDataSetChanged();
+      if (routes_.size() == 0)
+        closeDialog();
+    } // refresh
+
+    private void closeDialog() {
+      if (ad_ != null)
+        ad_.cancel();
+    } // closeDialog
+
+    public RouteSummary getRouteSummary(int localId) {
+      for(final RouteSummary r : routes_)
+        if(r.localId() == localId)
+          return r;
+      return null;
+    } // getRouteSummary
+
+    @Override
+    public int getCount() { return routes_.size(); }
+
+    @Override
+    public Object getItem(int position) { return routes_.get(position); }
+
+    @Override
+    public long getItemId(int position) { return routes_.get(position).localId(); }
+
+    @Override
+    public View getView(final int position, final View convertView, final ViewGroup parent) {
+      final RouteSummary summary = routes_.get(position);
+      final View v = inflater_.inflate(R.layout.storedroutes_item, parent, false);
+      viewRoute_.put(v, summary.localId());
+
+      final TextView n = (TextView)v.findViewById(R.id.route_title);
+
+      final String p = summary.plan();
+      final String plan = p.substring(0,1).toUpperCase() + p.substring(1);
+
+      n.setText(summary.title() + "\n" +
+          plan + " route, " +
+          Segment.formatter.total_distance(summary.distance()));
+
+      v.setOnClickListener(this);
+      v.setOnLongClickListener(this);
+      v.setOnCreateContextMenuListener(this);
+
+      return v;
+    } // getView
+
+    @Override
+    public void onClick(final View view) {
+      final int localId = viewRoute_.get(view);
+      openRoute(localId);
+    } // onClick
+
+    @Override
+    public boolean onLongClick(final View view) {
+      view.showContextMenu();
+      return true;
+    } // onClick
+
+    @Override
+    public void onCreateContextMenu(final ContextMenu menu,
+                                    final View view,
+                                    final ContextMenu.ContextMenuInfo contextMenuInfo) {
+      final MenuItem.OnMenuItemClickListener listener = new MenuItem.OnMenuItemClickListener() {
+        public boolean onMenuItemClick(final MenuItem item) {
+          RouteSummaryAdapter.this.onViewMenuClick(view, item);
+          return true;
+        } // onMenuItemClick
+      };
+      createMenuItem(menu, R.string.ic_menu_open).setOnMenuItemClickListener(listener);
+      createMenuItem(menu, R.string.ic_menu_rename).setOnMenuItemClickListener(listener);
+      createMenuItem(menu, R.string.ic_menu_delete).setOnMenuItemClickListener(listener);
+    } // onCreateContextMenu
+
+    private void onViewMenuClick(final View view, final MenuItem item) {
+      final int localId = viewRoute_.get(view);
+      final int menuId = item.getItemId();
+
+      if(R.string.ic_menu_open == menuId)
+        openRoute(localId);
+      if(R.string.ic_menu_rename == menuId)
+        renameRoute(localId);
+      if(R.string.ic_menu_delete == menuId)
+        deleteRoute(localId);
+    } // onMenuItemClick
+
+    //////////////////////////////////////////////
+    /////////////////////////////////////////////
+    private void openRoute(final int localId) {
+      Route.PlotStoredRoute(localId, context_);
+      closeDialog();
+    } // routeSelected
+
+    private void renameRoute(final int localId)
+    {
+      final RouteSummary route = getRouteSummary(localId);
+      Dialog.editTextDialog(context_, route.title(), "Rename",
+          new Dialog.UpdatedTextListener() {
+            @Override
+            public void updatedText(final String updated) {
+              Route.RenameRoute(localId, updated);
+              refresh();
+            } // updatedText
+          });
+    } // renameRoute
+
+    private void deleteRoute(final int localId) {
+      Route.DeleteRoute(localId);
+      refresh();
+    } // deleteRoute
+  } // class RouteSummaryAdaptor
+
+  private StoredRoutes() { }
+} // StoredRoutes
