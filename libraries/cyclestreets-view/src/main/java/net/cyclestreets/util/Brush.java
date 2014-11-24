372f316ccd43a2042f9b2c54f2e50d2bf20246c2
Jez Higgins <jez@jezuk.co.uk>
2014-11-24 19:33:27 +0000

    whitespace changes

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/Brush.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/Brush.java
@@ -5,41 +5,35 @@ import android.graphics.Typeface;
 import android.graphics.Paint.Align;
 import android.graphics.Paint.Style;
 
-public class Brush 
-{
-	static public Paint Grey = createFillBrush(255, 127, 127, 127);
-	static public Paint LightGrey = createFillBrush(255, 192, 192, 192);
-	static public Paint White = createFillBrush(255, 255, 255, 255);
-	static public Paint BlackOutline = createOutlineBrush(255,0,0,0);
+public class Brush {
+  public static Paint Grey = createFillBrush(255, 127, 127, 127);
+  public static Paint LightGrey = createFillBrush(255, 192, 192, 192);
+  public static Paint White = createFillBrush(255, 255, 255, 255);
+  public static Paint BlackOutline = createOutlineBrush(255,0,0,0);
 
-	static private Paint createFillBrush(final int a, final int r, final int g, final int b)
-	{
-	  return createBrush(a, r, g, b, Style.FILL_AND_STROKE);
-	} // createFillBrush
-	
-  static private Paint createOutlineBrush(final int a, final int r, final int g, final int b)
-  {
+  private static Paint createFillBrush(final int a, final int r, final int g, final int b) {
+    return createBrush(a, r, g, b, Style.FILL_AND_STROKE);
+  } // createFillBrush
+  
+  private static Paint createOutlineBrush(final int a, final int r, final int g, final int b) {
     final Paint brush = createBrush(a, r, g, b, Style.STROKE);
     brush.setStrokeWidth(0);
     return brush;
   } // createOutlineBrush
 
-  static private Paint createBrush(final int a, final int r, final int g, final int b, final Style style)
-	{
+  private static Paint createBrush(final int a, final int r, final int g, final int b, final Style style) {
     final Paint paint = new Paint();
     paint.setAntiAlias(true);
     paint.setStyle(style);
     paint.setARGB(a, r, g, b);    
     return paint;
-	} // createBrush
-	
-	static public Paint createTextBrush(final int size)
-	{
-	  return createTextBrush(size, 255, 255, 255, 255);
-	} // createTextBrush
-	
-	static public Paint createTextBrush(final int size, final int a, final int r, final int g, final int b)
-	{
+  } // createBrush
+  
+  public static Paint createTextBrush(final int size) {
+    return createTextBrush(size, 255, 255, 255, 255);
+  } // createTextBrush
+  
+  public static Paint createTextBrush(final int size, final int a, final int r, final int g, final int b)  {
     final Paint paint = createFillBrush(a, r, g, b);
 
     paint.setTextAlign(Align.CENTER);
@@ -47,5 +41,7 @@ public class Brush
     paint.setTextSize(size * 2);
     
     return paint;
-	} // createTextBrush
+  } // createTextBrush
+  
+  private Brush() { }
 } // Brushes
