1f23e028f6f2d6fe59dabf23831a0738d4abf921
Jez Higgins <jez@jezuk.co.uk>
2014-11-27 17:41:39 +0000

    Renamed

---
+++ cyclestreets/src/main/java/net/cyclestreets/CycleStreets.java
@@ -0,0 +1,54 @@
+package net.cyclestreets;
+
+import android.os.Bundle;
+
+import net.cyclestreets.ElevationProfileFragment;
+import net.cyclestreets.ItineraryFragment;
+import net.cyclestreets.MainNavDrawerActivity;
+import net.cyclestreets.MainSupport;
+import net.cyclestreets.PhotoMapFragment;
+import net.cyclestreets.PhotoUploadFragment;
+import net.cyclestreets.RouteAvailablePageStatus;
+import net.cyclestreets.RouteMapActivity;
+import net.cyclestreets.RouteMapFragment;
+import net.cyclestreets.WebPageFragment;
+
+public class CycleStreets extends MainNavDrawerActivity
+                          implements RouteMapActivity {
+  public void onCreate(final Bundle savedInstanceState)	{
+    MainSupport.switchMapFile(getIntent());
+
+    super.onCreate(savedInstanceState);
+
+    MainSupport.loadRoute(getIntent(), this);
+  } // onCreate
+
+  @Override
+  public void showMap() {
+    showPage(0);
+  } // showMap
+
+  @Override
+  protected void addDrawerItems() {
+    addDrawerFragment(R.string.route_map,
+                      R.drawable.ic_menu_mapmode,
+                      RouteMapFragment.class);
+    addDrawerFragment(R.string.itinerary,
+                      R.drawable.ic_menu_agenda,
+                      ItineraryAndElevationFragment.class,
+                      new RouteAvailablePageStatus());
+    addDrawerFragment(R.string.photomap,
+                      R.drawable.ic_menu_gallery,
+                      PhotoMapFragment.class);
+    addDrawerFragment(R.string.photo_upload,
+                      R.drawable.ic_menu_camera,
+                      PhotoUploadFragment.class);
+    addDrawerFragment(R.string.cyclestreets_blog,
+                      -1,
+                      WebPageFragment.class,
+                      WebPageFragment.initialiser("http://www.cyclestreets.net/blog/"));
+    addDrawerActivity(R.string.settings,
+                      android.R.drawable.ic_menu_preferences,
+                      SettingsActivity.class);
+  } // addDrawerItems
+} // CycleStreets
