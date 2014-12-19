a26e1b6d454e044adda5fc54d91f2eb1bc3095fb
Jez Higgins <jez@jezuk.co.uk>
2014-12-19 16:42:51 +0000

    Nice video controls display

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/DisplayPhoto.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/DisplayPhoto.java
@@ -12,13 +12,20 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.VideoView;
+
+import java.lang.ref.WeakReference;
 
 public final class DisplayPhoto {
   public static void launch(final Photo photo, final Context context) {
@@ -32,8 +39,11 @@ public final class DisplayPhoto {
   private DisplayPhoto() { }
 
   //////////////////////////////////////////
-  private static class VideoDisplay extends DisplayDialog implements MediaPlayer.OnPreparedListener {
+  private static class VideoDisplay
+      extends DisplayDialog
+      implements MediaPlayer.OnPreparedListener {
     private VideoView vv_;
+    private VideoControllerView controller_;
     private ProgressDialog pd_;
 
     VideoDisplay(final Photo photo, final Context context) {
@@ -45,6 +55,7 @@ public final class DisplayPhoto {
     @Override
     protected View loadLayout() {
       final View layout = View.inflate(context_, R.layout.showvideo, null);
+      controller_ = new VideoControllerView(layout.findViewById(R.id.videocontroller));
       vv_ = (VideoView)layout.findViewById(R.id.video);
       return layout;
     } // loadLayout
@@ -72,11 +83,8 @@ public final class DisplayPhoto {
     public void onPrepared(final MediaPlayer mediaPlayer) {
       pd_.dismiss();
 
-      //final MediaController mc = new MediaController(ad.getContext());
-      //mc.setAnchorView(vv_);
-      //mc.setMediaPlayer(vv_);
-      //vv_.setMediaController(mc);
-      //mc.show();
+      controller_.setMediaPlayer(vv_);
+      controller_.show();
     } // onPrepared
 
     private static String videoUrl(final Photo photo) {
@@ -163,9 +171,215 @@ public final class DisplayPhoto {
       final int device_width = wm.getDefaultDisplay().getWidth();
       final int height = (device_height > device_width)
           ? device_height / 10 * 5
-          : device_height / 10 * 7;
+          : device_height / 10 * 6;
       final int width = device_width;
       v.setLayoutParams(new LinearLayout.LayoutParams(width, height));
     } // sizeView
   } // DisplayDialog
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  private static class VideoControllerView {
+    private MediaController.MediaPlayerControl videoPlayer_;
+    private View controllerView_;
+    private SeekBar seekBar_;
+    private TextView timeElapsed_;
+    private TextView endTime_;
+    private boolean isDragging_;
+    private static final int SHOW_PROGRESS = 2;
+    private ImageButton pauseBtn_;
+    private ImageButton ffBtn_;
+    private ImageButton rewBtn_;
+    private Handler msgHandler_ = new MessageHandler(this);
+
+    public VideoControllerView(final View controllerView) {
+      controllerView_ = controllerView;
+      initControllerView(controllerView_);
+    } // VideoControllerView
+
+    public void setMediaPlayer(MediaController.MediaPlayerControl player) {
+      videoPlayer_ = player;
+      updatePausePlay();
+    } // setMediaPlayer
+
+    private void initControllerView(View v) {
+      pauseBtn_ = (ImageButton)v.findViewById(R.id.pause);
+      pauseBtn_.setOnClickListener(pauseListener_);
+
+      ffBtn_ = (ImageButton)v.findViewById(R.id.ffwd);
+      ffBtn_.setOnClickListener(ffListener_);
+
+      rewBtn_ = (ImageButton)v.findViewById(R.id.rew);
+      rewBtn_.setOnClickListener(rewListener_);
+
+      seekBar_ = (SeekBar)v.findViewById(R.id.mediacontroller_progress);
+      seekBar_.setOnSeekBarChangeListener(seekListener_);
+      seekBar_.setMax(1000);
+
+      timeElapsed_ = (TextView)v.findViewById(R.id.time_current);
+      endTime_ = (TextView)v.findViewById(R.id.time);
+    } // initControllerView
+
+    private void disableUnsupportedButtons() {
+      if (videoPlayer_ == null)
+        return;
+
+      try {
+        if (pauseBtn_ != null && !videoPlayer_.canPause())
+          pauseBtn_.setEnabled(false);
+        if (rewBtn_ != null && !videoPlayer_.canSeekBackward())
+          rewBtn_.setEnabled(false);
+        if (ffBtn_ != null && !videoPlayer_.canSeekForward())
+          ffBtn_.setEnabled(false);
+      } catch (IncompatibleClassChangeError ex) {
+      }
+    }
+
+    /**
+     * Show the controller on screen. .
+     */
+    public void show() {
+      if (!isShowing()) {
+        setProgress();
+        pauseBtn_.requestFocus();
+        disableUnsupportedButtons();
+      } // if ...
+
+      controllerView_.setVisibility(View.VISIBLE);
+      updatePausePlay();
+      msgHandler_.sendEmptyMessage(SHOW_PROGRESS);
+    } // show
+
+    private boolean isShowing() {
+      return (controllerView_.getVisibility() == View.VISIBLE);
+    } // isShowing
+
+    public void hide() {
+      controllerView_.setVisibility(View.GONE);
+    } // hide
+
+    private String formatTime(int timeMs) {
+      int totalSeconds = timeMs / 1000;
+
+      int seconds = totalSeconds % 60;
+      int minutes = (totalSeconds / 60) % 60;
+      int hours   = totalSeconds / 3600;
+
+      if (hours > 0)
+        return String.format("%d:%02d:%02d", hours, minutes, seconds);
+      else
+        return String.format("%02d:%02d", minutes, seconds);
+    } // formatTime
+
+    private int setProgress() {
+      if (videoPlayer_ == null || isDragging_)
+        return 0;
+
+      int position = videoPlayer_.getCurrentPosition();
+      int duration = videoPlayer_.getDuration();
+      if (duration > 0) {
+        long pos = 1000L * position / duration;
+        seekBar_.setProgress((int)pos);
+      } // if ...
+      int percent = videoPlayer_.getBufferPercentage();
+      seekBar_.setSecondaryProgress(percent * 10);
+
+      timeElapsed_.setText(formatTime(position));
+      endTime_.setText(formatTime(duration));
+
+      return position;
+    } // setProgress
+
+    private View.OnClickListener pauseListener_ = new View.OnClickListener() {
+      public void onClick(View v) { doPauseResume(); }
+    };
+
+    public void updatePausePlay() {
+      pauseBtn_.setImageResource(videoPlayer_.isPlaying() ? R.drawable.ic_media_pause : R.drawable.ic_media_play);
+    } // updatePausePlay
+
+    private void doPauseResume() {
+      if (videoPlayer_.isPlaying())
+        videoPlayer_.pause();
+      else
+        videoPlayer_.start();
+      updatePausePlay();
+    } // doPauseResume
+
+    private SeekBar.OnSeekBarChangeListener seekListener_ = new SeekBar.OnSeekBarChangeListener() {
+      public void onStartTrackingTouch(SeekBar bar) {
+        isDragging_ = true;
+
+        // By removing these pending progress messages we make sure
+        // that a) we won't update the progress while the user adjusts
+        // the seekbar and b) once the user is done dragging the thumb
+        // we will post one of these messages to the queue again and
+        // this ensures that there will be exactly one message queued up.
+        msgHandler_.removeMessages(SHOW_PROGRESS);
+      } // onStartTrackingTouch
+
+      public void onProgressChanged(SeekBar bar, int progress, boolean fromuser) {
+        if (!fromuser)
+          return;
+
+        long duration = videoPlayer_.getDuration();
+        long newposition = (duration * progress) / 1000L;
+        videoPlayer_.seekTo((int)newposition);
+        timeElapsed_.setText(formatTime((int) newposition));
+      } // onProgressChanged
+
+      public void onStopTrackingTouch(SeekBar bar) {
+        isDragging_ = false;
+        setProgress();
+        updatePausePlay();
+        // Ensure that progress is properly updated in the future,
+        // the call to show() does not guarantee this because it is a
+        // no-op if we are already showing.
+        msgHandler_.sendEmptyMessage(SHOW_PROGRESS);
+      } // onStopTrackingTouch
+    };
+
+    private View.OnClickListener rewListener_ = new View.OnClickListener() {
+      public void onClick(View v) {
+        int pos = videoPlayer_.getCurrentPosition();
+        pos -= 5000; // milliseconds
+        videoPlayer_.seekTo(pos);
+        setProgress();
+      }
+    };
+
+    private View.OnClickListener ffListener_ = new View.OnClickListener() {
+      public void onClick(View v) {
+        int pos = videoPlayer_.getCurrentPosition();
+        pos += 15000; // milliseconds
+        videoPlayer_.seekTo(pos);
+        setProgress();
+      }
+    };
+
+    private static class MessageHandler extends Handler {
+      private final WeakReference<VideoControllerView> view_;
+
+      MessageHandler(VideoControllerView view) {
+        view_ = new WeakReference<>(view);
+      }
+
+      @Override
+      public void handleMessage(Message msg) {
+        VideoControllerView view = view_.get();
+        if (view == null || view.videoPlayer_ == null)
+          return;
+
+        if (msg.what == SHOW_PROGRESS) {
+            int pos = view.setProgress();
+            if (!view.isDragging_ && view.isShowing() && view.videoPlayer_.isPlaying()) {
+              msg = obtainMessage(SHOW_PROGRESS);
+              sendMessageDelayed(msg, 1000 - (pos % 1000));
+            }
+            view.updatePausePlay();
+        } // if ...
+      } // handleMessage
+    } // class MessageHandler
+  } // class VideoViewController
 } // DisplayPhoto
+
+
