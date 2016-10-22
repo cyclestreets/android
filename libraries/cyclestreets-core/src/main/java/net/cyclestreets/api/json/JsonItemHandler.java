d7a241721e7c2298e40318158943c193ad8e107a
Oliver Lockwood <oliver.lockwood@cantab.net>
2016-10-22 12:51:32 +0200

    Remove all files in unused package net.cyclestreets.api.json

--- libraries/cyclestreets-core/src/main/java/net/cyclestreets/api/json/JsonItemHandler.java
+++
@@ -1,7 +0,0 @@
-package net.cyclestreets.api.json;
-
-import java.io.IOException;
-
-public interface JsonItemHandler {
-  void read(final String name, final JsonReader reader) throws IOException;
-}
