0c16991e00b57a2b12c790cbbd7dfa7f768a358b
andreig91 <andreig2191@gmail.com>
2017-02-15 21:38:55 -0500

    Added export as GPX feature. Closes #107
    A new option item "GPS file download" was added which allows the app user to download the GPX file
    which is already made available on the server.
    When the option is selected, the user is allowed to specify the name under which to save the GPX file.

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/GpsFileDownloader.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/GpsFileDownloader.java
@@ -3,49 +3,40 @@ package net.cyclestreets.util;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Environment;
 import net.cyclestreets.routing.Route;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class GpsFileDownloader {
 
-    public static final String GPX_FILE_EXTENSION = ".gpx";
-    public static final String ITINERARY_TOKEN = "%Itinerary%";
-    public static final String ROUTE_PLAN_TOKEN = "%Route_Plan%";
-    /*Sample gpx file URL:
-    https://cyclestreets.net/journey/52759941/cyclestreets52759941balanced.gpx
-    */
-    public static final String GPS_FILE_URL_PATH_FORMAT =
-            "https://cyclestreets.net/journey/" + ITINERARY_TOKEN + "/cyclestreets" +
-                    ITINERARY_TOKEN + ROUTE_PLAN_TOKEN + GPX_FILE_EXTENSION;
+  public static final String GPX_FILE_EXTENSION = ".gpx";
+  public static final String ITINERARY_TOKEN = "%Itinerary%";
+  public static final String ROUTE_PLAN_TOKEN = "%Route_Plan%";
 
-    public void downloadGPSFile(Context ctx) {
-        Uri gpsFileUrl = getGPSFileUrl();
-        DownloadManager.Request request = new DownloadManager.Request(gpsFileUrl);
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, getDestinationFilePath());
-        DownloadManager downloadManager = (DownloadManager) ctx.getSystemService(Context.DOWNLOAD_SERVICE);
-        downloadManager.enqueue(request);
-    }
+  // Sample gpx file URL: https://cyclestreets.net/journey/52759941/cyclestreets52759941balanced.gpx*/
+  public static final String GPS_FILE_URL_PATH_FORMAT =
+      "https://cyclestreets.net/journey/" + ITINERARY_TOKEN + "/cyclestreets" +
+          ITINERARY_TOKEN + ROUTE_PLAN_TOKEN + GPX_FILE_EXTENSION;
 
-    private Uri getGPSFileUrl() {
-        String itinerary = Integer.toString(Route.journey().itinerary());
-        String routePlan = Route.journey().plan();
-        String gpsFileUrl = GPS_FILE_URL_PATH_FORMAT.replaceAll(ITINERARY_TOKEN, itinerary);
-        gpsFileUrl = gpsFileUrl.replaceAll(ROUTE_PLAN_TOKEN, routePlan);
-        return Uri.parse(gpsFileUrl);
-    }
+  public void downloadGPSFile(Context ctx) {
+    Uri gpsFileUrl = getGPSFileUrl();
+    DownloadManager.Request request = new DownloadManager.Request(gpsFileUrl);
+    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, getDestinationFilePath());
+    DownloadManager downloadManager = (DownloadManager) ctx.getSystemService(Context.DOWNLOAD_SERVICE);
+    downloadManager.enqueue(request);
+  }
 
-    private String getDestinationFilePath() {
-        String itinerary = Integer.toString(Route.journey().itinerary());
-        String routePlan = Route.journey().plan();
-        return "cyclestreets" + itinerary + routePlan + GPX_FILE_EXTENSION;
-    }
+  private Uri getGPSFileUrl() {
+    String itinerary = Integer.toString(Route.journey().itinerary());
+    String routePlan = Route.journey().plan();
+    String gpsFileUrl = GPS_FILE_URL_PATH_FORMAT.replaceAll(ITINERARY_TOKEN, itinerary);
+    gpsFileUrl = gpsFileUrl.replaceAll(ROUTE_PLAN_TOKEN, routePlan);
+    return Uri.parse(gpsFileUrl);
+  }
+
+  private String getDestinationFilePath() {
+    String itinerary = Integer.toString(Route.journey().itinerary());
+    String routePlan = Route.journey().plan();
+    return "cyclestreets" + itinerary + routePlan + GPX_FILE_EXTENSION;
+  }
 }
