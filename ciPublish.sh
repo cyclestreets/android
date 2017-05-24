78fd8dc68173f14f7f5e8ddbe62bce8c11a65c42
Oliver Lockwood <oliver.lockwood@cantab.net>
2017-05-24 20:32:11 +0100

    Fix bash script issues

--- ciPublish.sh
+++ ciPublish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ev
 
-# We don't want to publish the artifact unless this is a non-PR build on master.
-if [ $CI = "true" ] && [ $TRAVIS_BRANCH == "master" ] && [ ! $TRAVIS_PULL_REQUEST = "true" ]; then
+if [ "$CI" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo "Running a non-PR build on master - publish artifact"
   echo "DUMMY STEP where we should publish the artifact we've built"
+else
+  echo "The artifact will not be published"
 fi
