23e96efefcfe754bf694996441c4bd6804a4c98e
Oliver Lockwood <oliver.lockwood@sky.uk>
2017-05-25 15:52:38 +0100

    Fix Travis decryption logic

--- ciLicense.sh
+++ ciLicense.sh
@@ -3,9 +3,9 @@ set -ev
 
 if [ "$CI" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Running a non-PR build on master - set up signing info"
-  openssl aes-256-cbc -K $encrypted_29b04bc3212e_key -iv $encrypted_29b04bc3212e_iv -in license.properties.enc -out license.properties -d
-  openssl aes-256-cbc -K $encrypted_29b04bc3212e_key -iv $encrypted_29b04bc3212e_iv -in signature.asc.enc -out signature.asc -d
-  openssl aes-256-cbc -K $encrypted_29b04bc3212e_key -iv $encrypted_29b04bc3212e_iv -in android-keystore.enc -out android-keystore -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.vNext/license.properties.enc -out cyclestreets.vNext/license.properties -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.vNext/signature.asc.enc -out cyclestreets.vNext/signature.asc -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.vNext/keystore.enc -out cyclestreets.vNext/keystore -d
 else
   echo "The compiled artifact will not be signed"
 fi
