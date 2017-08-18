4b489fbdd051b0da4c02e059477c702e8d9afeb8
Oliver Lockwood <oliver.lockwood@cantab.net>
2017-08-18 15:28:37 +0100

    Set up Play publishing plugin (#191)
    * Set up Play publishing plugin
    
    * Add encrypted PK12 API key
    
    * Use update p12 file
    
    * New Play API key
    
    * Update version numbering

--- ciPublish.sh
+++ ciPublish.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "$CI" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Running a non-PR build on master - publish artifact"
-  echo "DUMMY STEP where we should publish the artifact we've built"
+  ./gradlew publishApkRelease
 else
   echo "The artifact will not be published"
 fi
