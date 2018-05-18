e8a7655d8d172364d6e960eb601a51050d7fa109
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-05-18 08:57:51 +0100

    Fix typo in method name

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/Permissions.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/util/Permissions.java
@@ -5,12 +5,12 @@ import android.content.pm.PackageManager;
 
 public class Permissions {
   public static boolean verify(final Activity activity, final String permission) {
-    if (!hasPermssion(activity, permission))
+    if (!hasPermission(activity, permission))
       activity.requestPermissions(new String[]{ permission }, 1);
-    return hasPermssion(activity, permission);
+    return hasPermission(activity, permission);
   } // verify
 
-  private static boolean hasPermssion(final Activity activity, final String permission) {
+  private static boolean hasPermission(final Activity activity, final String permission) {
     return activity.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED;
   } // hasPermission
 } // class Permissions
