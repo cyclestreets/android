200f21f66f6c4d500f675073f961e9583ecb0692
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-07-10 08:21:25 +0100

    Track & expose total elevation gain and loss when building profile

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/routing/ElevationProfile.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/routing/ElevationProfile.java
@@ -7,6 +7,8 @@ public class ElevationProfile {
   private final LinkedList<Elevation> profile = new LinkedList<>();
   private int min = Integer.MAX_VALUE;
   private int max = Integer.MIN_VALUE;
+  private int totalElevationGain = 0;
+  private int totalElevationLoss = 0;
 
   void add(List<Elevation> segmentProfile) {
     int distanceUpToSegmentStart = profile.size() != 0 ? profile.getLast().distance() : 0;
@@ -23,11 +25,29 @@ public class ElevationProfile {
     min = Math.min(min, e.elevation());
     max = Math.max(max, e.elevation());
 
+    if (!profile.isEmpty()) {
+      Elevation last = profile.getLast();
+      int elevationGain = e.elevation() - last.elevation();
+      if (elevationGain > 0) {
+        totalElevationGain += elevationGain;
+      } else {
+        totalElevationLoss -= elevationGain;
+      }
+    }
+
     profile.add(e);
   }
 
   public Iterable<Elevation> profile() {
     return profile;
+  }
+
+  public int totalElevationGain() {
+    return totalElevationGain;
+  }
+
+  public int totalElevationLoss() {
+    return totalElevationLoss;
   }
 
   public int minimum() { return min; }
