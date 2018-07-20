c8c315d2d5400aaefd5d4c753f13b5a8e90ebdc9
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-07-20 18:03:06 +0100

    Revert "Add Material design icon library, use for navigate (liveride) and directions icons (#228)"
    This reverts commit d201e0192bb505f59e3c7adfe261e1b7a552918d.

--- libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/RouteMapFragment.java
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/RouteMapFragment.java
@@ -11,11 +11,12 @@ import net.cyclestreets.views.overlay.TapToRouteOverlay;
 import net.cyclestreets.routing.Journey;
 import net.cyclestreets.routing.Route;
 import net.cyclestreets.routing.Waypoints;
-import net.steamcrafted.materialiconlib.MaterialMenuInflater;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Color;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -78,10 +79,7 @@ public class RouteMapFragment extends CycleMapFragment implements Route.Listener
 
   @Override
   public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
-    MaterialMenuInflater
-            .with(getContext())
-            .setDefaultColor(Color.LTGRAY)
-            .inflate(R.menu.route_map, menu);
+    inflater.inflate(R.menu.route_map, menu);
     super.onCreateOptionsMenu(menu, inflater);
   }
 
