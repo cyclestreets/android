f3650ccc53c01d264591656ee47976978c193b99
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-08-02 14:16:08 +0100

    Use LinkedLists for Segments and Waypoints

--- libraries/cyclestreets-core/src/main/java/net/cyclestreets/routing/Waypoints.java
+++ libraries/cyclestreets-core/src/main/java/net/cyclestreets/routing/Waypoints.java
@@ -1,9 +1,6 @@
 package net.cyclestreets.routing;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import android.support.annotation.NonNull;
 import org.osmdroid.api.IGeoPoint;
@@ -17,31 +14,27 @@ public class Waypoints implements Iterable<IGeoPoint>
     return new Waypoints(start, end);
   }
 
-  private final List<IGeoPoint> waypoints;
+  private final LinkedList<IGeoPoint> waypoints = new LinkedList<>();
 
-  public Waypoints() {
-    waypoints = new ArrayList<>();
-  }
+  public Waypoints() {}
 
   private Waypoints(final IGeoPoint from, final IGeoPoint to) {
-    waypoints = new ArrayList<>();
     waypoints.add(from);
     waypoints.add(to);
   }
 
-  private Waypoints(final List<IGeoPoint> points) {
-    waypoints = points;
+  public Waypoints(final List<IGeoPoint> points) {
+    waypoints.addAll(points);
   }
 
   public int count() { return waypoints.size(); }
-  public boolean isEmpty() { return count() == 0; }
+  public boolean isEmpty() { return waypoints.isEmpty(); }
 
-  public IGeoPoint first() { return count() != 0 ? waypoints.get(0) : null; }
-  public IGeoPoint last() { return waypoints.get(count()-1); }
+  public IGeoPoint first() { return waypoints.isEmpty() ? null : waypoints.getFirst(); }
+  public IGeoPoint last() { return waypoints.getLast(); }
 
   public void add(final double lat, final double lon) { add(new GeoPoint(lat, lon)); }
   public void add(final IGeoPoint geopoint) { waypoints.add(geopoint); }
-  public void addAll(final List<IGeoPoint> geopoints) { waypoints.addAll(geopoints); }
 
   public IGeoPoint get(final int i) { return waypoints.get(i); }
 
