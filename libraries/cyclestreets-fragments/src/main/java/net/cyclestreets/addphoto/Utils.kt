f056b987733619bdb199744fdaf0f1b943e3978f
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-08-15 17:24:17 +0100

    Fix add photo issues (#262)
    * Move to a more widely-supported mechanism for capturing photos within the app
    
    * Ensure a user is logged in at the start of the Add Photo fragment journey
    
    * Request permissions on-demand for Add Photo
    
    * Delete previously captured images when about to create a new image
    
    * Minor improvements noted while self-reviewing

--- libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/addphoto/Utils.kt
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/addphoto/Utils.kt
@@ -1,21 +1,29 @@
 package net.cyclestreets.addphoto
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
+import android.os.Environment
 import android.provider.MediaStore
 import android.support.media.ExifInterface
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import net.cyclestreets.fragments.R
 import org.osmdroid.util.GeoPoint
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.io.File
 
 internal const val TAKE_PHOTO = 2
 internal const val CHOOSE_PHOTO = 3
 internal const val ACCOUNT_DETAILS = 4
+
+private const val PHOTO_FILE_PREFIX = "CS_PHOTO_"
 
 internal fun photoUploadMetaData(activity: Activity?): String {
     try {
@@ -39,16 +47,39 @@ internal fun backNextButtons(parentView: View,
     }
 }
 
+@SuppressLint("SimpleDateFormat")
+internal fun createImageFile(context: Context?): File {
+    deletePreviouslyCapturedImages(context)
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val imageFileName = "$PHOTO_FILE_PREFIX$timeStamp"
+    val file = File.createTempFile(imageFileName, ".jpg", storageDir(context))
+    Log.i(TAG, "Created temporary image file ${file.absolutePath}")
+    return file
+}
+
+private fun deletePreviouslyCapturedImages(context: Context?) {
+    val files = storageDir(context).listFiles { _, filename -> filename.matches(Regex("$PHOTO_FILE_PREFIX.*")) }
+    files.forEach {
+        Log.i(TAG, "Deleting previously captured image ${it.absolutePath}")
+        it.delete()
+    }
+}
+
+private fun storageDir(context: Context?): File {
+    return context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+}
+
 internal fun getImageFilePath(data: Intent, activity: Activity?): String {
     val selectedImage = data.data
     val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
 
-    val cursor = activity!!.contentResolver.query(selectedImage!!, filePathColumn, null, null, null)
+    var cursor: Cursor? = null
     try {
-        cursor!!.moveToFirst()
+        cursor = activity!!.contentResolver.query(selectedImage!!, filePathColumn, null, null, null)!!
+        cursor.moveToFirst()
         return cursor.getString(cursor.getColumnIndex(filePathColumn[0]))
     } finally {
-        cursor!!.close()
+        cursor?.close()
     }
 }
 
@@ -57,6 +88,7 @@ internal fun photoLocation(photoExif: ExifInterface): GeoPoint? {
     return if (coords != null) GeoPoint(coords[0], coords[1]) else null
 }
 
+@SuppressLint("SimpleDateFormat")
 internal fun photoTimestamp(photoExif: ExifInterface): String {
     var date = Date()
     try {
