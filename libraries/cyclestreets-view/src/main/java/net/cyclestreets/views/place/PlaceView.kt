609f34943c9fb0a2f2ee30d901df93b004f89acb
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-08-15 09:20:56 +0100

    Convert PlaceView and PlaceViewWithCancel to Kotlin

---
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/views/place/PlaceView.kt
@@ -0,0 +1,8 @@
+package net.cyclestreets.views.place
+
+import net.cyclestreets.view.R
+import android.content.Context
+import android.util.AttributeSet
+
+class PlaceView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
+        PlaceViewBase(context, R.layout.placetextview, attrs)
