fbec200952c5d37db2bf98be7f2f17d4c878e777
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-08-24 13:45:47 +0100

    Add a warning log

--- libraries/cyclestreets-core/src/main/java/net/cyclestreets/api/PhotoMarkers.kt
+++ libraries/cyclestreets-core/src/main/java/net/cyclestreets/api/PhotoMarkers.kt
@@ -5,10 +5,14 @@ import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.support.v4.content.res.ResourcesCompat
+import android.util.Log
 
 import net.cyclestreets.core.R
+import net.cyclestreets.util.Logging
 
 import java.util.HashMap
+
+private val TAG = Logging.getTag(PhotoMarkers::class.java)
 
 class PhotoMarkers(private val res: Resources) {
     private lateinit var defaultMarker: Drawable
@@ -34,11 +38,12 @@ class PhotoMarkers(private val res: Resources) {
                 markers[key] = marker
                 return marker
             } catch (e: Exception) {
+                Log.w(TAG, "Error while loading image $key into the cache")
                 markers[key] = defaultMarker
             }
         }
 
-        return markers[key]
+        return markers[key]!!
     }
 
     private fun mapMetaCat(mc: String): String {
