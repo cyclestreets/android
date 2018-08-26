ca318a52e2c45046b426153530704a4dd65c1dcd
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-08-26 21:39:37 +0100

    TurnIcons into Kotlin; simplify

--- libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/CycleStreetsAppSupport.java
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/CycleStreetsAppSupport.java
@@ -2,6 +2,8 @@ package net.cyclestreets;
 
 import net.cyclestreets.api.ApiClient;
 import net.cyclestreets.routing.Route;
+import net.cyclestreets.util.TurnIcons;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
@@ -11,6 +13,7 @@ public final class CycleStreetsAppSupport {
   private static String version_;
 
   public static void initialise(final Context context, final int prefsDefault) {
+    TurnIcons.initialise(context);
     CycleStreetsPreferences.initialise(context, prefsDefault);
     CycleStreetsNotifications.initialise(context);
 
