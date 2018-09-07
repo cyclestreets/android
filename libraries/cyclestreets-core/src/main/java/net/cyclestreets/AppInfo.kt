f89f9b0c4a2b02bf18262ea35b2a947a1ea0b482
Oliver Lockwood <oliver.lockwood@cantab.net>
2018-09-07 11:41:30 +0100

    Upgrade AppInfo to avoid deprecation warnings

--- libraries/cyclestreets-core/src/main/java/net/cyclestreets/AppInfo.kt
+++ libraries/cyclestreets-core/src/main/java/net/cyclestreets/AppInfo.kt
@@ -1,7 +1,10 @@
 package net.cyclestreets
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.os.Build
 
 private const val UNKNOWN = "unknown"
 
@@ -17,10 +20,20 @@ object AppInfo {
     private fun versionNameAndCode(context: Context): String {
         return try {
             val info = context.packageManager.getPackageInfo(context.packageName, 0)
-            String.format("%s/%s", info.versionName, info.versionCode.toString())
+            String.format("%s/%s", info.versionName, getVersionCode(info))
         } catch (e: PackageManager.NameNotFoundException) {
             // like this is going to happen
             UNKNOWN
+        }
+    }
+
+    @SuppressLint("NewApi")
+    @Suppress("deprecation")
+    private fun getVersionCode(info: PackageInfo): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            info.longVersionCode.toString()
+        } else {
+            info.versionCode.toString()
         }
     }
 }
