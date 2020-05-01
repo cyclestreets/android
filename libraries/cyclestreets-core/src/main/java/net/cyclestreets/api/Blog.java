896e46533ab1d359d07ea7577406f64de82df0f8
Jez Higgins <jez@jezuk.co.uk>
2020-05-01 16:45:11 +0100

    Flip cyclestreet.org links to https

--- libraries/cyclestreets-core/src/main/java/net/cyclestreets/api/Blog.java
+++ libraries/cyclestreets-core/src/main/java/net/cyclestreets/api/Blog.java
@@ -8,7 +8,7 @@ public class Blog
   private static final Blog NULL_BLOG;
   static {
     NULL_BLOG = new Blog(new ArrayList<BlogEntry>() {{
-      add(new BlogEntry("ERROR", "http://www.cyclestreets.org/news/", "Could not retrieve CycleStreets blog entries", ""));
+      add(new BlogEntry("ERROR", "https://www.cyclestreets.org/news/", "Could not retrieve CycleStreets blog entries", ""));
     }});
   }
   private List<BlogEntry> entries = new ArrayList<>();
