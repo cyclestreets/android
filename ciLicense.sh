1c6f478bcce9e5c2b1cfc3b30b46fb3f3cf07c53
Oliver Lockwood <oliver.lockwood@cantab.net>
2020-06-04 16:25:35 +0100

    Upgrade Gradle, and Play Publishing plugin (including to use JSON credentials)

--- ciLicense.sh
+++ ciLicense.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_REPO_SLUG" == "cyclestreets/android" ] &&[ "$CI" == "true" ] && [ 
   openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/license.properties.enc -out cyclestreets.app/license.properties -d
   openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/signature.asc.enc -out cyclestreets.app/signature.asc -d
   openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/keystore.enc -out cyclestreets.app/keystore -d
-  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/play-api-key.p12.enc -out cyclestreets.app/play-api-key.p12 -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/play-api-key.json.enc -out cyclestreets.app/play-api-key.json -d
   openssl aes-256-cbc -k $openssl_file_encryption_key -in libraries/cyclestreets-core/src/test/resources/cyclestreets-api.key.enc -out libraries/cyclestreets-core/src/test/resources/cyclestreets-api.key -d
 else
   echo "The compiled artifact will have a dummy signature"
