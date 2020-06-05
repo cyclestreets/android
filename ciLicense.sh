06317c992ca6c7f4424980c3ca03fcff621d5191
Oliver Lockwood <oliver.lockwood@cantab.net>
2020-06-05 10:28:52 +0100

    OpenSSL's defaults have changed.  Use '-md md5' for our decrypts so we don't have to re-encrypt everything.  Resubmit Play API key JSON encrypt.

--- ciLicense.sh
+++ ciLicense.sh
@@ -3,11 +3,11 @@ set -ev
 
 if [ "$TRAVIS_REPO_SLUG" == "cyclestreets/android" ] &&[ "$CI" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Running a non-PR build on master - set up signing info"
-  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/license.properties.enc -out cyclestreets.app/license.properties -d
-  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/signature.asc.enc -out cyclestreets.app/signature.asc -d
-  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/keystore.enc -out cyclestreets.app/keystore -d
-  openssl aes-256-cbc -k $openssl_file_encryption_key -in cyclestreets.app/play-api-key.json.enc -out cyclestreets.app/play-api-key.json -d
-  openssl aes-256-cbc -k $openssl_file_encryption_key -in libraries/cyclestreets-core/src/test/resources/cyclestreets-api.key.enc -out libraries/cyclestreets-core/src/test/resources/cyclestreets-api.key -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -md md5 -in cyclestreets.app/license.properties.enc -out cyclestreets.app/license.properties -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -md md5 -in cyclestreets.app/signature.asc.enc -out cyclestreets.app/signature.asc -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -md md5 -in cyclestreets.app/keystore.enc -out cyclestreets.app/keystore -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -md md5 -in cyclestreets.app/play-api-key.json.enc -out cyclestreets.app/play-api-key.json -d
+  openssl aes-256-cbc -k $openssl_file_encryption_key -md md5 -in libraries/cyclestreets-core/src/test/resources/cyclestreets-api.key.enc -out libraries/cyclestreets-core/src/test/resources/cyclestreets-api.key -d
 else
   echo "The compiled artifact will have a dummy signature"
   cp cyclestreets.app/license.properties.dummy cyclestreets.app/license.properties
