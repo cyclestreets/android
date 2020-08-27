625b86de060e3269b636c4629e4f0bf74c1a8d83
HilaryN <59799824+HilaryN@users.noreply.github.com>
2020-08-27 20:30:18 +0100

    Add overlay to mark Find Place on map (#431)
    * Add overlay to mark Find Place on map
    
    * Removed underscores.
    Made placeMarker Drawable non-nullable at time of instantiation.
    
    * Add What's New entry for this feature
    
    Co-authored-by: Oliver Lockwood <oliver.lockwood@cantab.net>

--- libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/CycleMapFragment.kt
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/CycleMapFragment.kt
@@ -147,7 +147,7 @@ open class CycleMapFragment : Fragment(), Undoable {
 
     private fun launchFindDialog() {
         FindPlace.launch(requireContext(), map!!.boundingBox) { place ->
-            map!!.centreOn(place, FINDPLACE_ZOOM_LEVEL)
+            map!!.centreOn(place, FINDPLACE_ZOOM_LEVEL, true)
         }
     }
 
