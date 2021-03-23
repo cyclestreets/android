e7dcab2e6359deba2daa288a42f18237108c76ab
Oliver Lockwood <oliver.lockwood@cantab.net>
2021-03-23 16:14:55 +0000

    461: fix tile loading regression bug

--- libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/CycleStreetsAppSupport.java
+++ libraries/cyclestreets-fragments/src/main/java/net/cyclestreets/CycleStreetsAppSupport.java
@@ -2,15 +2,23 @@ package net.cyclestreets;
 
 import net.cyclestreets.api.ApiClient;
 import net.cyclestreets.routing.Route;
+import net.cyclestreets.util.Logging;
 import net.cyclestreets.util.TurnIcons;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 public final class CycleStreetsAppSupport {
-  private static boolean isFirstRun_;
-  private static boolean isNew_;
-  private static String version_;
+
+  private static final String TAG = Logging.getTag(CycleStreetsAppSupport.class);
+
+  private static boolean isFirstRun;
+  private static boolean isNew;
+  private static String version;
+  private static Integer versionCode;
+  private static String previousVersion;
+  private static Integer previousVersionCode;
 
   public static void initialise(final Context context, final int prefsDefault) {
     TurnIcons.initialise(context);
@@ -21,32 +29,40 @@ public final class CycleStreetsAppSupport {
     ApiClient.INSTANCE.initialise(context);
     BlogState.INSTANCE.initialise(context);
 
-    version_ = version(context);
+    version = version(context);
+    versionCode = code(version);
+    previousVersion = previousVersion(context);
+    previousVersionCode = code(previousVersion);
 
-    isFirstRun_ = isFirstRun(context);
-    isNew_ = isNew(context, version_);
+    isFirstRun = isFirstRun(context);
+    isNew = !version.equals(previousVersion);
 
-    saveVersion(context, version_);
+    saveVersion(context, version);
+
+    migratePreferences(previousVersionCode, versionCode);
   }
 
-  public static String version() { return version_; }
-  public static boolean isNewVersion() { return isNew_; }
-  public static boolean isFirstRun() { return isFirstRun_; }
+  public static String version() { return version; }
+  public static boolean isNewVersion() { return isNew; }
+  public static boolean isFirstRun() { return isFirstRun; }
   public static void splashScreenSeen() {
-    isFirstRun_ = false;
-    isNew_ = false;
+    isFirstRun = false;
+    isNew = false;
   }
 
   private static String version(final Context context) {
     return "Version : " + AppInfo.INSTANCE.version(context);
   }
+  private static Integer code(String versionString) {
+    if (UNKNOWN.equals(versionString)) {
+      return 0;
+    }
+    String[] split = versionString.split("/");
+    return Integer.valueOf(split[split.length - 1]);
+  }
 
   private static boolean isFirstRun(final Context context) {
     return UNKNOWN.equals(previousVersion(context));
-  }
-  private static boolean isNew(final Context context, final String version) {
-    String prev = previousVersion(context);
-    return !version.equals(prev);
   }
   private static String previousVersion(final Context context) {
     return prefs(context).getString(VERSION_KEY, UNKNOWN);
@@ -66,6 +82,15 @@ public final class CycleStreetsAppSupport {
 
   private static final String VERSION_KEY = "previous-version";
   private static final String UNKNOWN = "unknown";
+
+  private static void migratePreferences(Integer previousVersionCode, Integer versionCode) {
+    Log.i(TAG, "Upgrading from " + previousVersion + " (" + previousVersionCode + ") to " + version + " (" + versionCode + ")");
+
+    if (previousVersionCode < 1621 && versionCode >= 1621) {
+      Log.i(TAG, "Clearing OSMDroid cache location after upgrade to target Android 10 (SDK 29) or higher changed accessible paths");
+      CycleStreetsPreferences.clearOsmdroidCacheLocation();
+    }
+  }
 
   private CycleStreetsAppSupport() { }
 }
