59591892ec2ce9b0c72150167f1999648a726542
HilaryN <59799824+HilaryN@users.noreply.github.com>
2021-08-05 19:31:38 +0100

    Add saveRoute param so LiveRide replans aren't saved (#483)

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/content/RouteData.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/content/RouteData.java
@@ -7,16 +7,20 @@ public class RouteData
   private final String name;
   private final String json;
   private final Waypoints points;
+  private final Boolean saveRoute;
 
   public RouteData(final String json,
                    final Waypoints points,
-                   final String name) {
+                   final String name,
+                   final Boolean saveRoute) {
     this.json = json;
     this.points = points;
     this.name = name;
+    this.saveRoute = saveRoute;
   }
 
   public String name() { return name; }
   public String json() { return json; }
   public Waypoints points() { return points; }
+  public Boolean saveRoute() { return saveRoute; }
 }
