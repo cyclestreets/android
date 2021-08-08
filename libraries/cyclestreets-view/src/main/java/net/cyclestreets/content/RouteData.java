f9504aae83f2353cde8dfb497977a69f59dbb249
HilaryN <hilarynewmark@hotmail.com>
2021-08-08 19:10:33 +0100

    Changed Boolean to primitive

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/content/RouteData.java
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/content/RouteData.java
@@ -7,12 +7,12 @@ public class RouteData
   private final String name;
   private final String json;
   private final Waypoints points;
-  private final Boolean saveRoute;
+  private final boolean saveRoute;
 
   public RouteData(final String json,
                    final Waypoints points,
                    final String name,
-                   final Boolean saveRoute) {
+                   final boolean saveRoute) {
     this.json = json;
     this.points = points;
     this.name = name;
@@ -22,5 +22,5 @@ public class RouteData
   public String name() { return name; }
   public String json() { return json; }
   public Waypoints points() { return points; }
-  public Boolean saveRoute() { return saveRoute; }
+  public boolean saveRoute() { return saveRoute; }
 }
