9499f2fb65f63d7ed99e36ba6ae861f0dbf38c74
HilaryN <hilarynewmark@hotmail.com>
2022-09-11 16:39:33 +0100

    Removed space from strings files. Refactored altText val.

--- libraries/cyclestreets-view/src/main/java/net/cyclestreets/views/overlay/RouteHighlightOverlay.kt
+++ libraries/cyclestreets-view/src/main/java/net/cyclestreets/views/overlay/RouteHighlightOverlay.kt
@@ -35,7 +35,7 @@ class RouteHighlightOverlay(context: Context, private val mapView: CycleMapView)
     private val prevButton: FloatingActionButton
     private val nextButton: FloatingActionButton
 
-    private val altText = context.getString(R.string.alt_route_details)
+    private val altRoutePrefix = context.getString(R.string.alt_route_details)
 
     init {
         val routeView = LayoutInflater.from(mapView.context).inflate(R.layout.route_view, null)
@@ -111,7 +111,7 @@ class RouteHighlightOverlay(context: Context, private val mapView: CycleMapView)
         }
         else {
             // The active segment is the Start segment and there is an alt journey.
-            summaryText = altJourney?.totalDistance()?.let { seg.summaryText(it, altJourney?.totalTime(), altText) }
+            summaryText = altJourney?.totalDistance()?.let { seg.summaryText(it, altJourney?.totalTime(), altRoutePrefix) }
                 .toString()
         }
 
