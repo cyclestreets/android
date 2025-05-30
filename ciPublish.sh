b0b097b39c7c4954ca0626838eaf7ef62c7e02cd
Jez Higgins <jez@jezuk.co.uk>
2025-05-30 09:51:16 +0100

    No need to specify JAVA_HOME anymore

--- ciPublish.sh
+++ ciPublish.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "$CI" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Running a non-PR build on master - publish artifact"
-  ./gradlew publishReleaseApk -Dorg.gradle.java.home=${JAVA17_HOME} -x lint
+  ./gradlew publishReleaseApk -x lint
 else
   echo "The artifact will not be published"
 fi
